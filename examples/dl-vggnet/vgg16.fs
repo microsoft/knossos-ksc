dd3ef92ccfc96942b408ea4e878056356a88a7dd
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2018-11-21 13:57:00 +0000

    Improve vgg16, still outline code.

--- examples/dl-vggnet/vgg16.fs
+++ examples/dl-vggnet/vgg16.fs
@@ -1,64 +1,62 @@
 
-let inline rand p = 
-  Array.init 2 (fun i -> 1.1)
-
 type Vec = float[]
 type Mat = Vec[]
 type Tensor3 = Mat[]
 
+let inline rand p = 
+  Mat (Array.init 2 (fun i -> 1.1))
 
+let W = 128
+let H = 128
 
 let conv2 (w:Mat) (x:Vec) = x
 let mmul (a:Mat) (b:Vec) = b
 let relu (x:Vec) = x
 let dropout (x:Vec) = x
 let softmax (x:Vec) = x
+let max_pooling_2d (x:Vec) = x
+let foldr (f: 'y -> 'x -> 'x ) (ys: 'y[]) (x : 'x) =  Array.foldBack f ys x
+let (<||) f g x y = f (g x y)         // >
+let repeat n f = seq { yield (f 1) }
 
 type vgg_weights = { 
-  Wconv: Mat seq   // Weight matrices for convolutional layers
-  Wfull: Mat[2]    // Weight matrices for fully connected layers
+  Wconv: Mat[][]   // Weight matrices for convolutional layers
+  Wfull: Mat[]     // Weight matrices for fully connected layers
 }
 
 // A vgg16 network (applied wlog to 1D images).  
 // Apply weights to image x
 let vgg16 weights x =
-    let vggblk Ws x = foldr (conv2 >> relu) W x |> max_pooling_2d
-    foldr vggblk weights.Wc x |>
-    mmul weights.Wfc.[0] |> relu |> dropout |>
-    mmul weights.Wfc.[1] |> relu |> dropout |>
+    let vggblk Ws x = foldr (relu <|| conv2) Ws x |> max_pooling_2d
+    foldr vggblk weights.Wconv x |>
+    mmul weights.Wfull.[0] |> relu |> dropout |>
+    mmul weights.Wfull.[1] |> relu |> dropout |>
     softmax
 
 // Network "architecture" is defined by the
 // function 'vgg16' and shapes of the weight matrices
-let vggblk_init n_channels n_convs = seq {
-    yield rand (128*128, n_channels, 3, 1, 1)
-    repeat (n_convs - 1) (fun _ -> 
-      yield rand (n_channels, n_channels, 3, 1, 1)
-    )
-  }
+let vggblk_init n_channels n_convs =
+    seq { 
+      yield rand (W*H, n_channels, 3, 1, 1)
+      yield rand (n_channels, n_channels, 3, 1, 1) 
+      if n_convs = 3 then
+        yield rand (n_channels, n_channels, 3, 1, 1)
+    } |> Seq.toArray
 
 let vgg16_init x_size = {
-    Wc = seq {
+    Wconv = [|
       vggblk_init  64 2
       vggblk_init 128 2
       vggblk_init 256 3 
       vggblk_init 512 3 
       vggblk_init 512 3 
       vggblk_init 512 3 
-    }; 
-    Wfc = [|
+    |] 
+    Wfull = [|
       rand (None, 4096)
       rand (4096, 4096)
     |]
   }
 
 
-
-let xvggblk_init n_channels n_convs =
-    seq { 
-      yield rand (None, n_channels, 3, 1, 1)
-      yield rand (n_channels, n_channels, 3, 1, 1) 
-      if n_convs = 3 then
-        yield rand (n_channels, n_channels, 3, 1, 1)
-    }
 
