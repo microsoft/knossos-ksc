96fafb524ecd9fb59e2c9a030b39fc23062f3306
Tom Ellis <tom.ellis@microsoft.com>
2019-03-05 16:40:11 +0000

    Pull out error-warnings into a variable

--- test/builds/build_linux.sh
+++ test/builds/build_linux.sh
@@ -1,4 +1,6 @@
 GHC=$1
 
-$GHC -main-is test -isrc/ksc -Werror -Wunused-local-binds -Wunused-matches -Wunused-imports -Wunused-top-binds -Wmissing-signatures src/ksc/Main.hs
-$GHC -main-is profile -o profile -isrc/ksc -Werror -Wunused-local-binds -Wunused-matches -Wunused-imports -Wunused-top-binds -Wmissing-signatures src/ksc/Main.hs
+ERRORS="-Wunused-local-binds -Wunused-matches -Wunused-imports -Wunused-top-binds -Wmissing-signatures"
+
+$GHC -main-is test -isrc/ksc -Werror $ERRORS src/ksc/Main.hs
+$GHC -main-is profile -o profile -isrc/ksc -Werror $ERRORS src/ksc/Main.hs
