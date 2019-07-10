9c1d565c485701d87998648f205929b417fb9c72
Tom Ellis <tom.ellis@microsoft.com>
2019-07-10 14:34:56 +0100

    Upgrade to GHC 8.6.3

--- test/builds/install_linux.sh
+++ test/builds/install_linux.sh
@@ -9,10 +9,10 @@ sudo add-apt-repository ppa:hvr/ghc -y
 echo ----- apt-get update -----
 sudo apt-get update
 echo ----- apt-get install gcc ghc -----
-sudo apt-get install gcc-7 g++-7 ghc-8.4.4
+sudo apt-get install gcc-7 g++-7 ghc-8.6.3
 echo ----- perftools -----
 sudo apt-get install libgoogle-perftools-dev google-perftools
 echo ----- cabal update -----
 cabal update
 echo ----- cabal install -----
-cabal install --with-compiler /opt/ghc/8.4.4/bin/ghc-8.4.4 hspec parsec mtl hashable
+cabal install --with-compiler /opt/ghc/8.6.3/bin/ghc-8.6.3 hspec parsec mtl hashable
