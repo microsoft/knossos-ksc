278c917db7c62a7c3840c3a492c5c8769d85d9e6
Andrew Fitzgibbon <awf@microsoft.com>
2019-07-19 17:26:00 +0100

    Improve error handling, don't land in a subdir

--- f2k.ps1
+++ f2k.ps1
@@ -1,9 +1,15 @@
 # Script to run f2k on fs files
 param([switch]$del=$false, [string]$out)
 
-if ($del -and (test-path $out)) {
-    write-host "Deleting $out"
-    del $out
+if (test-path $out) {
+	if ($del) {
+		write-host "Deleting $out"
+		del $out
+	} else {
+		throw "Output file $out exists, not deleting.  Call with '-del' to force delete."
+	}
 }
 cd src\f2k
 dotnet run f2k "..\..\$out" ($args | % { "../../$_" })
+cd ../..
+
