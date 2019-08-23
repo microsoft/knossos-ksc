1a43179fee58f4b71f107d4f61471c23dbaadfe7
Tom Ellis <tom.ellis@microsoft.com>
2019-08-23 16:12:43 +0100

    WIP linear ad

--- src/ksc/Ksc/LinearAD.hs
+++ src/ksc/Ksc/LinearAD.hs
@@ -4,6 +4,7 @@ module Ksc.LinearAD where
 
 import qualified Annotate                      as A
 import           Parse                          ( parseF )
+import qualified Prim
 import           KMonad                         ( runKM
                                                 , banner
                                                 , KM
@@ -36,6 +37,9 @@ demoN decls = do
 
   let linear_defs = map lineariseD anf_defs
   disp "Linear original definition" env linear_defs
+
+  let diff_defs = map differentiateD linear_defs
+  disp "Derivative" env diff_defs
 
 disp :: String -> LU.GblSymTab -> [L.TDef] -> KM ()
 disp what env decls = do
@@ -73,3 +77,94 @@ lineariseE = \case
   var@( L.Var{} ) -> var
   call@(L.Call{}) -> call
   v               -> error ("unexpected " ++ L.render (L.ppr v))
+
+differentiateD :: L.TDef -> L.TDef
+differentiateD tdef@(L.Def { L.def_rhs = L.UserRhs rhs }) = L.Def
+  { L.def_fun    = L.def_fun tdef
+  , L.def_args   = args ++ [rev r]
+  , L.def_res_ty = res_ty
+  , L.def_rhs    = L.UserRhs d_rhs
+  }
+ where
+  (fwd, r, trace, rev') = differentiateE rhs
+  d_rhs = (fwd . rev' trace . L.Tuple) (L.Var r : map (L.Var . rev) args)
+  args                  = L.def_args tdef
+  res_ty                = L.TypeTuple (L.def_res_ty tdef : map L.typeof args)
+differentiateD _ = error "differentiateD"
+
+differentiateE
+  :: L.TExpr
+  -> (L.TExpr -> L.TExpr, L.TVar, [L.TVar], [L.TVar] -> L.TExpr -> L.TExpr)
+differentiateE = \case
+  L.Dup (v1, v2) (L.Var v) body ->
+    ( L.Dup (v1, v2) (L.Var v) . body'
+    , r
+    , xs
+    , \xs' -> f xs' . L.Let v (Prim.pAdd (L.Var v1) (L.Var v2))
+    )
+    where (body', r, xs, f) = differentiateE body
+  L.Let v (L.Call (L.TFun t (L.Fun (L.UserFun op))) [L.Var a1, L.Var a2]) body
+    -> case op of
+      "add" ->
+        ( L.Let
+            v
+            (L.Call (L.TFun t (L.Fun (L.UserFun "add"))) [L.Var a1, L.Var a2])
+          . body'
+        , r
+        , xs
+        , \xs' -> f xs' . L.Dup (rev a1, rev a2) (L.Var (rev v))
+        )
+      "mul" ->
+        ( L.Dup (a1', a2') (L.Var a1)
+          . L.Dup (renameTVar a2 (++ "$1"), a2) (L.Var a2)
+          . L.Let
+              v
+              (L.Call (L.TFun t (L.Fun (L.UserFun "mul"))) [L.Var a1, L.Var a2])
+          . body'
+        , r
+        , a1 : a2 : xs
+        , \(a1' : a2' : xs') ->
+          f xs'
+            . L.Dup (rev v1, rev v2) (L.Var (rev v))
+            . L.Let (rev a1) (Prim.pMul (L.Var (rev v1)) (L.Var a2'))
+            . L.Let (rev a2) (Prim.pMul (L.Var (rev v2)) (L.Var a1'))
+        )
+      "div"
+        -> ( L.Dup (a1', a2') (L.Var a1)
+             . L.Dup (renameTVar a2 (++ "$1"), a2) (L.Var a2)
+             . L.Let
+                 v
+                 (L.Call (L.TFun t (L.Fun (L.UserFun "div")))
+                         [L.Var a1, L.Var a2]
+                 )
+             . body'
+           , r
+           , a1 : a2 : xs
+           , \(a1' : a2' : xs') ->
+             f xs'
+               . L.Dup (rev v1, rev v2) (L.Var (rev v))
+               . L.Let (rev a1) (Prim.pDiv (L.Var (rev v1)) (L.Var a2'))
+               . L.Let
+                   (rev a2)
+                   (Prim.pNeg
+                     (Prim.pDiv (L.Var (rev v2))
+                                (Prim.pMul (L.Var a1') (L.Var a1'))
+                     )
+                   )
+           )
+      s -> error ("Unexpected " ++ s)
+   where
+    (body', r, xs, f) = differentiateE body
+    a1'               = renameTVar a1 (++ "$1")
+    a2'               = renameTVar a2 (++ "$1")
+    v1                = undefined
+    v2                = undefined
+
+  _ -> error "differentiate"
+
+renameTVar :: L.TVar -> (String -> String) -> L.TVar
+renameTVar (L.TVar t (L.Simple s)) f = L.TVar t (L.Simple (f s))
+renameTVar _                       _ = error "renameTVar"
+
+rev :: L.TVar -> L.TVar
+rev = flip renameTVar (++ "$r")
