046729a7ab27892c245dcec071e8a5c0bae4be02
Tom Ellis <tom-git@jaguarpaw.co.uk>
2019-08-24 18:22:12 +0100

    Question about ANF

--- src/ksc/ANF.hs
+++ src/ksc/ANF.hs
@@ -54,6 +54,7 @@ anfE subst (Let v r e)    = do { r' <- anfE subst r
                                ; emit v' r'
                                ; anfE subst' e }
 anfE _ubst (Dup{})        = error "anfE Dup unimplemented"
+-- Why isn't b anf'd?
 anfE subst (If b t e)     = atomise =<<
                             do { t' <- anfExpr subst t
                                ; e' <- anfExpr subst e
