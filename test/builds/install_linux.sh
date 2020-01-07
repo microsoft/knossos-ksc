fc184e5b7802665be678be102225036fea55c0ff
Tom Ellis <tom.ellis@microsoft.com>
2020-01-07 15:11:33 +0000

    Update to GHC 8.6.5 and Cabal 3.0 for Linux build

--- test/builds/install_linux.sh
+++ test/builds/install_linux.sh
@@ -9,13 +9,13 @@ sudo add-apt-repository ppa:hvr/ghc -y
 echo ----- apt-get update -----
 sudo apt-get update
 echo ----- apt-get install gcc ghc -----
-sudo apt-get install gcc-7 g++-7 ghc-8.6.3 cabal-install-2.4
+sudo apt-get install gcc-7 g++-7 ghc-8.6.5 cabal-install-3.0
 echo ----- perftools -----
 sudo apt-get install libgoogle-perftools-dev google-perftools
 echo ----- cabal update -----
-/opt/cabal/2.4/bin/cabal update
+/opt/cabal/3.0/bin/cabal update
 echo ----- cabal install -----
-/opt/cabal/2.4/bin/cabal v1-install --with-compiler /opt/ghc/8.6.3/bin/ghc-8.6.3 hspec parsec mtl hashable
+/opt/cabal/3.0/bin/cabal v1-install --with-compiler /opt/ghc/8.6.5/bin/ghc-8.6.5 hspec parsec mtl hashable
 echo ------ futhark -----
 wget https://futhark-lang.org/releases/futhark-0.11.2-linux-x86_64.tar.xz
 tar -xvaf futhark-0.11.2-linux-x86_64.tar.xz
