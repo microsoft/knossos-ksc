0ed42ea9bf78436be4d96375540037506bddf5b5
Tom Ellis <tom.ellis@microsoft.com>
2020-01-07 17:16:27 +0000

    Build test executable with cabal only

--- test/builds/build_linux.sh
+++ test/builds/build_linux.sh
@@ -4,5 +4,4 @@ GHC=$1
 
 ERRORS="-Wall -Wno-name-shadowing -Wno-unticked-promoted-constructors -Wno-type-defaults -Wno-unused-do-bind -Wincomplete-uni-patterns -Wincomplete-record-updates"
 
-$GHC -main-is test -isrc/ksc -Werror $ERRORS src/ksc/Main.hs
 $GHC -main-is profile -o profile -isrc/ksc -Werror $ERRORS src/ksc/Main.hs
