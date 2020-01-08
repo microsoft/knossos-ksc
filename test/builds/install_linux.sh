7e788020605570c9bc38297573822f9b08b145c2
Tom Ellis <tom.ellis@microsoft.com>
2020-01-08 10:42:35 +0000

    Build with Cabal only
    We don't need to keep the legacy build script around anymore

--- test/builds/install_linux.sh
+++ test/builds/install_linux.sh
@@ -14,8 +14,6 @@ echo ----- perftools -----
 sudo apt-get install libgoogle-perftools-dev google-perftools
 echo ----- cabal update -----
 /opt/cabal/3.0/bin/cabal update
-echo ----- cabal install -----
-/opt/cabal/3.0/bin/cabal v1-install --with-compiler /opt/ghc/8.6.5/bin/ghc-8.6.5 hspec parsec mtl hashable
 echo ------ futhark -----
 wget https://futhark-lang.org/releases/futhark-0.11.2-linux-x86_64.tar.xz
 tar -xvaf futhark-0.11.2-linux-x86_64.tar.xz
