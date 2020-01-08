ca05d5114147cde8551445067d7ee112e0d90392
Tom Ellis <tom.ellis@microsoft.com>
2020-01-08 10:42:35 +0000

    Build with Cabal only
    We don't need to keep the legacy build script around anymore

--- test/builds/build_linux.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-GHC=$1
-
-ERRORS="-Wall -Wno-name-shadowing -Wno-unticked-promoted-constructors -Wno-type-defaults -Wno-unused-do-bind -Wincomplete-uni-patterns -Wincomplete-record-updates"
-
-$GHC -o ksc -isrc/ksc -Werror $ERRORS src/ksc/Main.hs
