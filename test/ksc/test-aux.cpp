14483ae5a01cc0a43f020f8af38a050bd362a2ef
Tom Ellis <tom.ellis@microsoft.com>
2020-01-09 18:16:03 +0000

    Remove old, unused C++ files

--- test/ksc/test-aux.cpp
+++
@@ -1,9 +0,0 @@
-
-// Test that inclusion of knossos.h in multiple files doesn't cause doubly-defined symbols
-#include "knossos.h"
-
-void test_ctor()
-{
-	std::vector<double> v{ 0.1, 0.2, 0.3 };
-	ks::vec<double> vv{ v };
-}
