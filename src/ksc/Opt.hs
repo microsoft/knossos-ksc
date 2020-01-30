e59257c66f7ad1798dabab61ffd56ece6be5458f
Tom Ellis <tom.ellis@microsoft.com>
2020-01-30 10:21:48 +0000

    Remove warning exception for incomplete uni patterns

--- src/ksc/Opt.hs
+++ src/ksc/Opt.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
-
 -- Copyright (c) Microsoft Corporation.
 -- Licensed under the MIT license.
 {-# LANGUAGE TypeFamilies, DataKinds, FlexibleInstances, LambdaCase,
