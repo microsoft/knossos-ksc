419d94a7ff027c1d4a97890f162a19adf596c524
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2020-02-09 10:23:41 +0000

    Clean up, improve docDefined jl2ks(function, argtypes)

--- src/j2k/j2k.jl
+++ src/j2k/j2k.jl
@@ -2,41 +2,47 @@
 using Zygote
 using IRTools
 import Base.show
+import IRTools: isconditional
 
 ### TODOS
-# 1. Nice printing :)
-# 2. Instead of env_args, collect free vars of expressions, and pass those down explicitly
-#    (e.g. now all basic blocks get passed all incoming args, which is messy, and will 
-#    cause a few redunant derivative calculations.  Of course Knossos can just rewrite 
-#    them away, but it's nice to remove obvious cruft.
-# 3. Emit fully monomorphized (essentially name-mangled) function names, e.g. "mul@Float64,Array{Float64}"
-# 4. Handle unhandled constructs
+# 1. Need to propagate types 
+#    (still some Anys in basic block defs)
+# 2. Nice printing :)
 
+# Newline constant for s-expr printing
 nl = "\n"
+tab = "\t"
 
-blockid(id::Int) = string("b",id)
-
-struct Ex 
-    args :: Array{Any}
+# Simple s-expression structure
+struct SExpr
+    terms :: Array{Any}
 end
 
-head(e) = e.args[1]
+# Return the head of an s-expression
+head(e) = e.terms[1]
+tail(e) = SExpr(e.terms[2:end])
 
-function show(io::IO, ex::Ex)
+# Print an s-expression
+function show(io::IO, ex::SExpr)
     print(io, "(")
-    args = ex.args
-    if length(args) > 0
-        for a in args[1:end-1]
+    terms = ex.terms
+    if length(terms) > 0
+        for a in terms[1:end-1]
             print(io, a)
             print(io, " ")
         end
-        print(io, args[end])
+        print(io, terms[end])
     end
     print(io, ")")
 end
 
-sexp(args...) = Ex([args...])
+sexp(terms...) = SExpr([terms...])
 
+## IR utilities
+blockid(id::Int) = string("b",id)
+
+    
+## make_sexp converts IR to s-expressions
 function make_sexp(x :: Any)
     sexp("[UNK", typeof(x), ":", x, "]")
 end
@@ -46,7 +52,7 @@ function make_sexp(x :: Number)
 end
 
 function make_sexp(x :: IRTools.Variable)
-    string("v",x)
+    string("_", x.id)
 end
 
 function make_sexp(x :: GlobalRef)
@@ -70,7 +76,7 @@ function make_let(x :: IRTools.Variable, st :: IRTools.Statement, body)
     if isnothing(x)
         sexp("nil", ex)
     else
-        sexp("let", sexp(string("v%", x.id),ex), nl, body)
+        sexp("let", sexp(make_sexp(x),ex), nl, body)
     end
 end
 
@@ -85,44 +91,44 @@ function make_lets(let_args, body)
     make_let(x, st, body)
 end
 
+function make_if(cond, texpr, fexpr)
+    sexp("if", make_sexp(cond), nl,
+         tab, texpr, nl,
+         tab, fexpr)
+end
+
 # struct IRTools.Branch
 #     condition :: Any
 #     block     :: Int64
 #     args      :: Array{Any,1}
 
-function make_sexp_br(env_args, id :: Int, br :: IRTools.Branch)
+function make_sexp_branch(id :: Int, br :: IRTools.Branch)
     if br.block == 0
         sexp(br.args...)
     else
-        sexp(blockid(br.block), map(head, env_args)..., br.args...)
+        sexp(blockid(br.block), br.args...)
     end
 end
 
-function make_if(cond, t, f)
-    sexp("if", cond, nl,
-         t, nl,
-         f)
+function make_sexp_branches(ir :: IRTools.IR, id :: Int, br :: IRTools.Branch, tail...)
+    # Must be conditional if tail nonempty
+    @assert isconditional(br)
+    make_if(br.condition, 
+            make_sexp_branch(id, br), 
+            make_sexp_branches(ir, id, tail...))
 end
 
-function make_sexp_brs(env_args, id :: Int, br :: IRTools.Branch)
+function make_sexp_branches(ir :: IRTools.IR, id :: Int, br :: IRTools.Branch)
     # The last branch in a list
-    if isnothing(br.condition)
-        # unconditional... 
-        make_sexp_br(env_args, id, br)
-    else
+    if isconditional(br)
         # if conditional, is either the br or a fallthrough
         make_if(br.condition,
-                sexp(blockid(id+1), map(head, env_args)...), 
-                make_sexp_br(env_args, id, br))
+                sexp(blockid(id+1), IRTools.arguments(ir.blocks[id])), 
+                make_sexp_branch(id, br))
+    else
+        # unconditional... 
+        make_sexp_branch(id, br)
     end
-end
-
-function make_sexp_brs(env_args, id :: Int, br :: IRTools.Branch, tail...)
-    # Must be conditional if tail nonempty
-    @assert !isnothing(br.condition)
-    make_if(br.condition, 
-            make_sexp_br(env_args, id, br), 
-            make_sexp_brs(env_args, id, tail...))
 end
 
 function block_args(b :: IRTools.Block)
@@ -130,96 +136,76 @@ function block_args(b :: IRTools.Block)
     [sexp(a,":",t) for (a,t) in ats]
 end
 
-function make_sexp(env_args, env_args2, b :: IRTools.Block)
-    args = sexp(env_args..., block_args(b)...)
-
-    if false
-        for (x,t) in b
-            if x == undef
-                println("T>>>", t)
-            else
-                println(x," = ", t)
-            end
-        end
+# struct IRTools.Block
+#    arguments   4: (%1, %2)
+#    argtypes
+#    statements   %1 = e
+#    branches     br n if e     -- if e, call block n, else fallthrough
+function make_def(is_toplevel :: Bool, name, b :: IRTools.Block)
+    args = sexp(block_args(b)...)
+    if is_toplevel
+        # Strip the method name for top-level functions
+        args = tail(args) 
     end
 
-    branches = make_sexp_brs(env_args2, b.id, IRTools.branches(b)...)
+    branches = make_sexp_branches(b.ir, b.id, IRTools.branches(b)...)
 
     let_args = [(x,t) for (x,t) in b if x != undef]
     body = make_lets(let_args, branches)
-            
-    se = sexp("def",blockid(b.id), 
-              "Any",
-              args, nl,
-              body)
-    se
+
+    sexp("def",name, 
+            "Any", nl, tab,
+            args, nl,
+            body)
 end
 
+# Make s-expression for method f
+# Generate a top-level function for each basic block,
+# Top level: ir is a list of blocks
 function make_sexps(ir :: IRTools.IR)
     blocks = IRTools.blocks(ir)
-    env_args = block_args(blocks[1])
 
+    # Emit blocks n:2 in reverse
     for b in reverse(blocks[2:end])
-        se = make_sexp(env_args, env_args, b)
+        se = make_def(false, blockid(b.id), b)
         println(se,"\n")
     end
-    se = make_sexp([], env_args, blocks[1])
+    # Emit block 1
+    f = IRTools.argtypes(blocks[1])[1].val
+    se = make_def(true, string(f), blocks[1])
     println(se)
 end
 
 ###################
-### Test
+### Test make_sexp
+
 
 print("---\n")
 f(x) = cos(x) * x
 
 sumsq(xs) = sum(xs.^2)
 
-function foo1(as,b)
-    if length(as) > 1
-        y = [a*sin(a) for a in as] .* f(b)
+function foo1(as :: Vector{Float64}, b :: Float64)
+    p = length(as)
+    p = p - 1
+    if p > 0
+        if as[1] > 0
+            y = [sin(a) for a in as] .* f(b)
+        else
+            y = [1.1*p, foo1(as[2:end], 1.1)]
+        end
     else
         y = -as.*f(b)
     end
-    f(sumsq(y)) + 5
+    f(sumsq(y)) + 5.5555
 end
 
-ir = IR(IRTools.typed_meta(Tuple{typeof(foo1),Array{Float64,1},Float64}))
-IRTools.expand!(ir)
-println(ir)
-println("--------------------")
-make_sexps(ir)
-
-#------------------------------------------
-const Float = Float64
-
-function make_f(a :: Float)
-    b = 2a
-    t -> sin(b*t)
+function jl2ks(f, argtypes)
+    meta = IRTools.typed_meta(Tuple{typeof(f),argtypes...})
+    ir = IRTools.IR(meta)
+    IRTools.expand!(ir)
+    make_sexps(ir)
 end
 
-function h(as, xs)
-    fs = map(make_f, as)
-    vals = map(map, fs, map(cos, xs))
-    sum(vals)
-end
+jl2ks(foo1, (Vector{Float64},Float64))
 
-function h1(as, xs)
-    sum([g2(as[i],xs[i]) for i = 1:length(as)])
-end
-
-as = [x + 0.1 for x in 1:4]
-xs = [sqrt(x) for x in 1:4]
-
-map(make_f, as)
-
-println("h = ",h(as, xs))
-println(Zygote.pullback(h, as, xs))
-
-println("h1 = ", h1(as, xs))
-println(Zygote.gradient(h1, as, xs))
-
-ir = IR(IRTools.typed_meta(Tuple{typeof(h),Array{Float64,1},Array{Float64,1}}, 
-            optimize=true))
-#IRTools.expand!(ir)
-make_sexps(ir)
