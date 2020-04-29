c8d004661fd598da55fb9c9532995d2e388d7950
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-04-29 12:32:53 +0100

    Simpler count

--- src/ksc/LangUtils.hs
+++ src/ksc/LangUtils.hs
@@ -55,9 +55,7 @@ isSingleton [x] = True
 isSingleton _   = False
 
 count :: (a->Bool) -> [a] -> Int
-count f [] = 0
-count f (x:xs) | f x       = 1 + count f xs
-               | otherwise = count f xs
+count f = length . filter f
 
 -----------------------------------------------
 --     Functions over expressions
