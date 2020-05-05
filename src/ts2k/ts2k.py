fdb3b37a8ebb6352b432f7bb057b7cbded067c2d
Colin Gravill <colin@gravill.com>
2020-05-05 15:48:33 +0100

    Support literals from TorchScript

--- src/ts2k/ts2k.py
+++ src/ts2k/ts2k.py
@@ -1,10 +1,18 @@
 import torch
 import sexpdata
 
+import os
+
+# TODO: Take output as argument
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../../obj/test/ts2k/ts2k_test.ks')
+
+output = open(filename, "w")
 
 # Newline constant for s-expr printing
 nl = "\n"
 tab = "\t"
+
 
 # @torch.jit.script
 # def sqr(x):
@@ -18,72 +26,100 @@ tab = "\t"
 def f(x):
     return x + 4.0
 
+
 @torch.jit.script
 def main():
+    t = torch.tensor([[1.2, 3.4, 5.6], [2.3, 4.5, 6.7]])
+    t2 = f(t)
     print("Hello world from TorchScript -> Knossos!")
 
-#https://github.com/pytorch/pytorch/blob/master/torch/csrc/jit/OVERVIEW.md
-#https://pytorch.org/docs/master/jit.html#interpreting-graphs
-#https://github.com/pytorch/pytorch/blob/8fe2a5e91b79e3f9b6f6c632fdf7f39ec3bf4fca/torch/csrc/jit/ir/ir.h
 
-print("#| -------- Graph ----------")
-print(f.graph)
-print("-------- |#")
+# https://github.com/pytorch/pytorch/blob/master/torch/csrc/jit/OVERVIEW.md
+# https://pytorch.org/docs/master/jit.html#interpreting-graphs
+# https://github.com/pytorch/pytorch/blob/8fe2a5e91b79e3f9b6f6c632fdf7f39ec3bf4fca/torch/csrc/jit/ir/ir.h
 
-
-# (def foo1 Any 
-#  	 ((%2 : Array{Float64,1}) (%3 : Float64)) 
-#  (let (_4 (Base.broadcasted Main.__anon__.* _2 4.0)) 
-#  (let (_5 (Base.materialize _4)) 
-#  (%5))))
-
-# print(type(f)) 
-
-# print(f.code) 
+output.write("#| -------- Graph ----------")
+output.write(str(f.graph))
+output.write(str(main.graph))
+output.write("-------- |#")
+output.write("\n")
 
 symbolLook = {
-    "Tensor": [sexpdata.Symbol("Vec"), sexpdata.Symbol("Float")]# float vs integer?
+    "Tensor": [sexpdata.Symbol("Vec"), sexpdata.Symbol("Float")]  # float vs integer?
 }
 
 
 def make_arg(input):
-    #[input.debugName(), " :", input.type()]
+    # [input.debugName(), " :", input.type()]
     return [
         sexpdata.Symbol(input.debugName()),
         sexpdata.Symbol(":"),
         symbolLook[str(input.type())]
     ]
 
+
+# prim::Constant
+# prim::ListConstruct
+# prim::ListConstruct
+# prim::ListConstruct
+# aten::tensor
+# prim::CallFunction
+# prim::Print
+
+def managleDebugName(name):
+    return "_" + name
+
+def make_constant(node):
+
+    value = node.outputsAt(0)
+
+    try:
+        possibleLiteral = value.toIValue()
+        if possibleLiteral is None:
+            literal = 0.0
+        elif possibleLiteral == 0:
+            literal = 0.0
+        else:
+            literal = possibleLiteral
+    except RuntimeError:
+        literal = "FUNCTIONCALL"
+
+    return [sexpdata.Symbol("\n"), sexpdata.Symbol("_" + value.debugName()), literal]    
+
+def make_print(node):
+    mangled_id = managleDebugName(node.inputsAt(0).debugName())
+    return [sexpdata.Symbol("print"), sexpdata.Symbol(mangled_id)]
+
+
+def make_default(node):
+    print("TODO:" + node.kind() )
+    return sexpdata.Symbol("")
+
+
+lookups = {
+    'prim::Constant': make_constant,
+    'prim::Print': make_print
+}
+
+
+def make_node(node):
+    return lookups.get(node.kind(), make_default)(node)
+
+
 def ts2ks(function):
+    name = sexpdata.Symbol(function.name)
 
-    name = sexpdata.Symbol("main") #sexpdata.Symbol(function.name)
+    args = [make_arg(item) for item in function.graph.inputs() ]
 
-    [make_arg(item) for item in function.graph.inputs() ]
-    args = list(map(make_arg, function.graph.inputs()))
-    body = [sexpdata.Symbol("print"), "\n" "Hello world from TorchScript -> Knossos!" "\n"]
-    wholeExp = [sexpdata.Symbol('def'), name, sexpdata.Symbol("Integer"), args, body]
+    stuff = list(function.graph.nodes())[0:-1]
 
-    function.graph.inputs()
+    binds = [make_node(node) for node in stuff]
+    op = make_node(list(function.graph.nodes())[-1])
 
-    # for node in function.graph.nodes():
+    body = [sexpdata.Symbol("\n"), sexpdata.Symbol("let"), binds, sexpdata.Symbol("\n"), op]
 
-    #     name = function.name
+    whole_exp = [sexpdata.Symbol('def'), name, sexpdata.Symbol("Integer"), args, body]
 
-    #     print(node)
-
-    #     print(node.kind())
-
-    #     for inpu in node.inputs():
-    #         print(inpu)
-
-    print(sexpdata.dumps(wholeExp))
+    output.write(sexpdata.dumps(whole_exp))
 
 ts2ks(main)
-
-
-#print(dir(e.graph.nodes))
-#print(e.code)
-
-
-# print(f.graph)
-# print(f.code)
