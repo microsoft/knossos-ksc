65634e7a0410fdc06d85580322e59ac4ff99c9ba
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-06-29 12:45:16 +0100

    Explain how to build the hash executable

--- src/Hash/Main.hs
+++ src/Hash/Main.hs
@@ -1,3 +1,13 @@
+-- To build this, follow the "Compiling the ksc executable"
+-- instructions from the ksc README, with the following changes
+--
+-- 1. Run it in the "Hash" directory
+--
+-- 2. The hash executable is called "hash" so if it still exists you
+-- need to "rm hash" instead of "rm ksc".
+--
+--    https://github.com/microsoft/knossos-ksc#compiling-the-ksc-executable
+
 module Main where
 
 import Benchmark
