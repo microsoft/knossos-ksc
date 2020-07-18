35f3da293f336807bf09a42fd5c85a47cd640875
Andrew Fitzgibbon <awf@microsoft.com>
2020-07-18 08:28:16 +0100

    Move Assert to its own file

--- mlir/include/Parser/AST.h
+++ mlir/include/Parser/AST.h
@@ -11,42 +11,7 @@
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Casting.h"
 
-#include <stdio.h>
-#include <execinfo.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <iostream>
-
-inline void ks_backtrace()
-{
-  void *array[10];
-
-  // get void*'s for all entries on the stack
-  size_t size = backtrace(array, sizeof array / sizeof array[0]);
-
-  // print out all the frames to stderr
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-}
-
-struct asserter {
-  std::ostream* s;
-  asserter() {
-    s = &std::cerr;
-    *s << "\nASSERT FAIL: ";
-  }
-  ~asserter() {
-    std::cerr << std::endl;
-    ks_backtrace();
-    assert(false); 
-  }
-  template <class T>
-  std::ostream& operator<<(const T& t) { return *s << t; }
-};
-
-// ASSERT(p) << msg;
-#define ASSERT(p) if (p) ; else asserter()
-
+#include "Assert.h"
 
 namespace Knossos {
 namespace AST {
