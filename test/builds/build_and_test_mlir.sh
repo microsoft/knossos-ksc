dc274bf49b45c81fe1c34b743a20aa7d45c95c69
Colin Gravill <colin.gravill@microsoft.com>
2020-07-21 11:16:02 +0100

    Tidy up scripts and test cache

--- test/builds/build_and_test_mlir.sh
+++ test/builds/build_and_test_mlir.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Known good git commit for LLVM
-# GOOD_HASH=462b960de8c
+# Make sure to keep in sync with .yml build scripts
 GOOD_HASH=e03394c6a6f
 if [ "$1" != "" ]; then
   GOOD_HASH="$1"
@@ -10,8 +10,6 @@ fi
 # Clone LLVM and build MLIR
 # This configuration requires clang+lld
 if [ ! -d llvm-project ]; then
-  #git clone git@github.com:llvm/llvm-project.git
-  # Trialing HTTPS to ease building on hosted build agents
   git clone https://github.com/llvm/llvm-project
 fi
 cd llvm-project
