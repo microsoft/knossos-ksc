de474223bdb799e91ee994cee0f3a7b240a2efd8
Andrew Fitzgibbon <awf@microsoft.com>
2020-07-30 08:02:33 +0000

    Improve consistency of cmake between README, linux. windows

---
+++ mlir/run-cmake-in-llvm.ps1
@@ -0,0 +1,15 @@
+# Powershell syntax, use 
+#   tr '`' '\\' < mlir/run-cmake-in-llvm.batsh | sh -x
+# to run in bash
+
+cmake ../llvm`
+    -G Ninja`
+    -DCMAKE_BUILD_TYPE=Debug`
+    -DCMAKE_C_COMPILER=clang`
+    -DCMAKE_CXX_COMPILER=clang++`
+    -DLLVM_ENABLE_PROJECTS=mlir`
+    -DLLVM_BUILD_EXAMPLES=OFF`
+    -DLLVM_TARGETS_TO_BUILD="X86;NVPTX"`
+    -DLLVM_ENABLE_ASSERTIONS=ON`
+    -DLLVM_ENABLE_LLD=ON`
+    -DMLIR_CUDA_RUNNER_ENABLED=ON
