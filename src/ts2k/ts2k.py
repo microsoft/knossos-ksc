dcc295728676b74d507c900045794cdfd39ee7b4
Colin Gravill <1099725+cgravill@users.noreply.github.com>
2020-08-06 22:26:46 +0100

    Apply https://pypi.org/project/black/ to TS2K (#343)

--- src/ts2k/ts2k.py
+++ src/ts2k/ts2k.py
@@ -6,10 +6,14 @@ import sys
 import inspect
 import argparse
 
-parser = argparse.ArgumentParser(description='Convert TorchScript to Knossos IR')
-parser.add_argument('--input_file', required=True, help='TorchScript [file_path].py')
-parser.add_argument('--output_file', required=True, help='Knossos IR [file_path].ks')
-parser.add_argument('--generate_edef', action='store_true', help="Generate edef, otherwise default to pass-through")
+parser = argparse.ArgumentParser(description="Convert TorchScript to Knossos IR")
+parser.add_argument("--input_file", required=True, help="TorchScript [file_path].py")
+parser.add_argument("--output_file", required=True, help="Knossos IR [file_path].ks")
+parser.add_argument(
+    "--generate_edef",
+    action="store_true",
+    help="Generate edef, otherwise default to pass-through",
+)
 args = parser.parse_args()
 
 input_file_path = args.input_file
@@ -17,7 +21,7 @@ filename = args.output_file
 generate_edef = args.generate_edef
 
 directory = os.path.dirname(filename)
-if (directory != ""):
+if directory != "":
     os.makedirs(directory, exist_ok=True)
 output = open(filename, "w")
 
@@ -38,7 +42,9 @@ add_edef = "(edef addATEN (Vec (Vec Float)) ((Vec (Vec Float)) Float))"
 
 # load all TorchScript methods in target modules
 # (various limitations, local sibling modules)
-dynamicMembers = inspect.getmembers(dynamicModule, predicate=lambda v : type(v) == torch.jit.ScriptFunction)
+dynamicMembers = inspect.getmembers(
+    dynamicModule, predicate=lambda v: type(v) == torch.jit.ScriptFunction
+)
 
 # https://github.com/pytorch/pytorch/blob/master/torch/csrc/jit/OVERVIEW.md
 # https://pytorch.org/docs/master/jit.html#interpreting-graphs
@@ -60,18 +66,24 @@ output.write("\n")
 output.write("\n")
 
 symbolLook = {
-    "Tensor": [sexpdata.Symbol("Vec"), [sexpdata.Symbol("Vec"), sexpdata.Symbol("Float")]]  # float vs integer? also determine rank instead of hardcode
+    "Tensor": [
+        sexpdata.Symbol("Vec"),
+        [sexpdata.Symbol("Vec"), sexpdata.Symbol("Float")],
+    ]  # float vs integer? also determine rank instead of hardcode
 }
+
 
 def mangleDebugName(name):
     return "_" + name.replace(".", "_")
+
 
 def make_arg(input):
     return [
         sexpdata.Symbol(mangleDebugName(input.debugName())),
         sexpdata.Symbol(":"),
-        symbolLook[str(input.type())]
+        symbolLook[str(input.type())],
     ]
+
 
 def make_constant(node):
 
@@ -88,24 +100,33 @@ def make_constant(node):
     except RuntimeError:
         literal = "FUNCTIONCALL"
 
-    return [sexpdata.Symbol("\n"), sexpdata.Symbol("_" + value.debugName()), literal]    
+    return [sexpdata.Symbol("\n"), sexpdata.Symbol("_" + value.debugName()), literal]
+
 
 def make_print(node):
     mangled_id = mangleDebugName(node.inputsAt(0).debugName())
     return sexpdata.Symbol("print"), sexpdata.Symbol(mangled_id)
 
+
 def make_list_init_inner(values, i, rescue):
     value = values[0]
     rhs = [sexpdata.Symbol("assert"), sexpdata.Symbol("false"), sexpdata.Symbol(rescue)]
-    if (len(values) > 1):
+    if len(values) > 1:
         rhs = make_list_init_inner(values[1:], i + 1, rescue)
-    return [sexpdata.Symbol("if"), [sexpdata.Symbol("eq"), sexpdata.Symbol("ni"), i], sexpdata.Symbol(mangleDebugName(value.debugName())), rhs]
+    return [
+        sexpdata.Symbol("if"),
+        [sexpdata.Symbol("eq"), sexpdata.Symbol("ni"), i],
+        sexpdata.Symbol(mangleDebugName(value.debugName())),
+        rhs,
+    ]
+
 
 def make_list_init(values):
     # We may switch to vector literals later: https://github.com/microsoft/knossos-ksc/issues/310
     # in the meantime here's a quick-and-dirty translation to chained if
     # CAUTION: if it goes out of range it uses the first value!
     return make_list_init_inner(values, 0, mangleDebugName(values[0].debugName()))
+
 
 def make_list(node):
     value = node.outputsAt(0)
@@ -122,12 +143,13 @@ def make_list(node):
                 [
                     sexpdata.Symbol("ni"),
                     sexpdata.Symbol(":"),
-                    sexpdata.Symbol("Integer")
+                    sexpdata.Symbol("Integer"),
                 ],
-                make_list_init(list(node.inputs()))
-            ]
-        ]
-    ]    
+                make_list_init(list(node.inputs())),
+            ],
+        ],
+    ]
+
 
 def make_tensor(node):
     # tensors aren't explicitly modelled in Knossos yet, leave them as identify over a (jagged) list for now
@@ -136,48 +158,57 @@ def make_tensor(node):
     return [
         sexpdata.Symbol("\n"),
         sexpdata.Symbol(mangleDebugName(value.debugName())),
-        sexpdata.Symbol(mangleDebugName(node.inputsAt(0).debugName()))]
+        sexpdata.Symbol(mangleDebugName(node.inputsAt(0).debugName())),
+    ]
+
 
 def make_add(node):
     value = node.outputsAt(0)
     if generate_edef:
-         return [
+        return [
             sexpdata.Symbol("\n"),
             sexpdata.Symbol(mangleDebugName(value.debugName())),
             [
                 sexpdata.Symbol("addATEN"),
                 sexpdata.Symbol(mangleDebugName(node.inputsAt(0).debugName())),
-                sexpdata.Symbol(mangleDebugName(node.inputsAt(1).debugName()))
-            ]
+                sexpdata.Symbol(mangleDebugName(node.inputsAt(1).debugName())),
+            ],
         ]
     else:
-        print("WARNING: aten::add just returns unmodified tensor, consider using --generate_edef")
+        print(
+            "WARNING: aten::add just returns unmodified tensor, consider using --generate_edef"
+        )
         return [
             sexpdata.Symbol("\n"),
             sexpdata.Symbol(mangleDebugName(value.debugName())),
-            sexpdata.Symbol(mangleDebugName(node.inputsAt(0).debugName()))
+            sexpdata.Symbol(mangleDebugName(node.inputsAt(0).debugName())),
         ]
+
 
 def make_return(node):
     mangled_id = mangleDebugName(node.inputsAt(0).debugName())
     return sexpdata.Symbol(mangled_id)
 
+
 def make_callfunction(node):
     value = node.outputsAt(0)
     function_name_constant = node.inputsAt(0).node()
-    input1 = node.inputsAt(1) # TODO: get all inputs instead of just first, 0th is function name itself
+    input1 = node.inputsAt(
+        1
+    )  # TODO: get all inputs instead of just first, 0th is function name itself
     function_name = function_name_constant.s("name")
     return [
         sexpdata.Symbol("\n"),
         sexpdata.Symbol(mangleDebugName(value.debugName())),
-        [ # get a function call - brackets are escaped by sexpdata
+        [  # get a function call - brackets are escaped by sexpdata
             sexpdata.Symbol(function_name),
             sexpdata.Symbol(mangleDebugName(input1.debugName())),
-        ]
+        ],
     ]
 
+
 def make_default(node):
-    print("WARNING, unimplmented node kind:" + node.kind() )
+    print("WARNING, unimplmented node kind:" + node.kind())
     return sexpdata.Symbol("")
 
 
@@ -190,55 +221,68 @@ def make_default(node):
 # prim::Print
 
 lookups = {
-    'prim::Constant': make_constant,
-    'prim::Print': make_print,
-    'prim::ListConstruct': make_list,
-    'aten::tensor': make_tensor,
-    'aten::add': make_add,
-    'prim::Return': make_return,
-    'prim::CallFunction': make_callfunction
+    "prim::Constant": make_constant,
+    "prim::Print": make_print,
+    "prim::ListConstruct": make_list,
+    "aten::tensor": make_tensor,
+    "aten::add": make_add,
+    "prim::Return": make_return,
+    "prim::CallFunction": make_callfunction,
 }
 
 
 def translate_node(node):
     return lookups.get(node.kind(), make_default)(node)
 
+
 if generate_edef:
     output.write(add_edef)
     output.write("\n")
     output.write("\n")
 
+
 def ts2ks(function):
     name = sexpdata.Symbol(function.name)
 
-    args = [make_arg(item) for item in function.graph.inputs() ]
+    args = [make_arg(item) for item in function.graph.inputs()]
 
     all_nodes = list(function.graph.nodes())
 
-    binds = [translate_node(node) for node in all_nodes if node.kind() != 'prim::Print']
-    print_count = sum(1 for node in all_nodes if node.kind() == 'prim::Print')
-    
+    binds = [translate_node(node) for node in all_nodes if node.kind() != "prim::Print"]
+    print_count = sum(1 for node in all_nodes if node.kind() == "prim::Print")
+
     # HACK: if last operation is print, we want that otherwise it's a return value.
     # need to think about interaction between imperative Python and pure Knossos
-    if (list(function.graph.nodes())[-1].kind() == 'prim::Print'):
-        if (print_count > 1):
-            print("WARNING: multiple print statements used, only final one currently translated")
+    if list(function.graph.nodes())[-1].kind() == "prim::Print":
+        if print_count > 1:
+            print(
+                "WARNING: multiple print statements used, only final one currently translated"
+            )
         op = translate_node(list(function.graph.nodes())[-1])
         return_type = sexpdata.Symbol("Integer")
     else:
-        if (print_count > 0):
-            print("WARNING: print statement currently only supported as final operation")
+        if print_count > 0:
+            print(
+                "WARNING: print statement currently only supported as final operation"
+            )
         return_node = function.graph.return_node()
         op = translate_node(return_node)
         return_type = symbolLook[str(return_node.inputsAt(0).type())]
 
-    body = [sexpdata.Symbol("\n"), sexpdata.Symbol("let"), binds, sexpdata.Symbol("\n"), op]
+    body = [
+        sexpdata.Symbol("\n"),
+        sexpdata.Symbol("let"),
+        binds,
+        sexpdata.Symbol("\n"),
+        op,
+    ]
 
-    whole_exp = [sexpdata.Symbol('def'), name, return_type, args, body]
+    whole_exp = [sexpdata.Symbol("def"), name, return_type, args, body]
 
     output.write(sexpdata.dumps(whole_exp))
+
 
 for (name, member) in dynamicMembers:
     ts2ks(member)
     output.write("\n\n")
-    #print(name)
+    # print(name)
