878010f56d2d2833dc5ee8818469545f545d1261
Colin Gravill <colin@gravill.com>
2020-10-20 10:30:00 +0100

    Separate out values check Otherwise adds noise to ts->ks

--- test/ts2k/gmm.py
+++ test/ts2k/gmm.py
@@ -105,32 +105,32 @@ def Qtimesx(Qdiag, L, x):
 
 
 # @torch.jit.script
-def gmm_objective(alphas, means, icf, x, wishart_gamma, wishart_m):
-    n = x.shape[0]
-    d = x.shape[1]
+# def gmm_objective(alphas, means, icf, x, wishart_gamma, wishart_m):
+#     n = x.shape[0]
+#     d = x.shape[1]
 
-    Qdiags = torch.exp(icf[:, :d])
-    sum_qs = torch.sum(icf[:, :d], 1)
-    Ls = torch.stack([constructL(d, curr_icf) for curr_icf in icf])
+#     Qdiags = torch.exp(icf[:, :d])
+#     sum_qs = torch.sum(icf[:, :d], 1)
+#     Ls = torch.stack([constructL(d, curr_icf) for curr_icf in icf])
 
-    xcentered = torch.stack(tuple(x[i] - means for i in range(n)))
-    Lxcentered = Qtimesx(Qdiags, Ls, xcentered)
-    sqsum_Lxcentered = torch.sum(Lxcentered ** 2, 2)
-    inner_term = alphas + sum_qs - 0.5 * sqsum_Lxcentered
-    lse = logsumexpvec(inner_term)
-    slse = torch.sum(lse)
+#     xcentered = torch.stack(tuple(x[i] - means for i in range(n)))
+#     Lxcentered = Qtimesx(Qdiags, Ls, xcentered)
+#     sqsum_Lxcentered = torch.sum(Lxcentered ** 2, 2)
+#     inner_term = alphas + sum_qs - 0.5 * sqsum_Lxcentered
+#     lse = logsumexpvec(inner_term)
+#     slse = torch.sum(lse)
 
-    CONSTANT = -n * d * 0.5 * math.log(2 * math.pi)
-    return (
-        CONSTANT
-        + slse
-        - n * logsumexp(alphas)
-        + log_wishart_prior(d, wishart_gamma, wishart_m, sum_qs, Qdiags, icf)
-    )
+#     CONSTANT = -n * d * 0.5 * math.log(2 * math.pi)
+#     return (
+#         CONSTANT
+#         + slse
+#         - n * logsumexp(alphas)
+#         + log_wishart_prior(d, wishart_gamma, wishart_m, sum_qs, Qdiags, icf)
+#     )
 
 
 @torch.jit.script
-def gmm_objective2(alphas, means, icf, x, wishart_gamma, wishart_m):
+def gmm_objective(alphas, means, icf, x, wishart_gamma, wishart_m):
     n = x.shape[0]
     d = x.shape[1]
 
@@ -157,33 +157,3 @@ def gmm_objective2(alphas, means, icf, x, wishart_gamma, wishart_m):
         - n * logsumexp(alphas)
         + log_wishart_prior(d, wishart_gamma, wishart_m, sum_qs, Qdiags, icf)
     )
-
-print(gmm_objective2.graph)
-
-# extracted from adbench test
-alphas = torch.tensor(
-    [-0.6490, 1.1812, -0.7585], dtype=torch.float64, requires_grad=True
-)
-means = torch.tensor(
-    [[0.0923, 0.1863], [0.3456, 0.3968], [0.5388, 0.4192]],
-    dtype=torch.float64,
-    requires_grad=True,
-)
-icf = torch.tensor(
-    [[0.5864, -0.8519, 0.8003], [-1.5094, 0.8759, -0.2428], [0.1668, -1.9654, -1.2701]],
-    dtype=torch.float64,
-    requires_grad=True,
-)
-x = torch.tensor([[1.1752, 2.0292]], dtype=torch.float64)
-wishart_gamma = torch.tensor(1.0, dtype=torch.float64)
-wishart_m = torch.tensor(0.0, dtype=torch.float64)
-
-result = gmm_objective2(alphas, means, icf, x, wishart_gamma, wishart_m)
-
-print(result)
-
-print(means.grad)
-
-result.backward(retain_graph = True)
-
-print(means.grad)
