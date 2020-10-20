a30101cbb2f59dcef9fba6f2042b944e7576e66b
Colin Gravill <colin@gravill.com>
2020-10-20 11:49:22 +0100

    Allow sibling imports More typing for clearer errors on bad arguments

--- src/ts2k/ts2k.py
+++ src/ts2k/ts2k.py
@@ -5,6 +5,7 @@ import importlib.util
 import sys
 import inspect
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description="Convert TorchScript to Knossos IR")
 parser.add_argument("--input_file", required=True, help="TorchScript [file_path].py")
@@ -17,25 +18,45 @@ parser.add_argument(
 args = parser.parse_args()
 
 input_file_path = args.input_file
-filename = args.output_file
+output_file_path = args.output_file
 generate_edef = args.generate_edef
 
-directory = os.path.dirname(filename)
-if directory != "":
-    os.makedirs(directory, exist_ok=True)
-output = open(filename, "w")
+input_directory = os.path.dirname(input_file_path)
+
+output_directory = os.path.dirname(output_file_path)
+if output_directory != "":
+    os.makedirs(output_directory, exist_ok=True)
+output = open(output_file_path, "w")
 
 # Newline constant for s-expr printing
 nl = "\n"
 tab = "\t"
 
+from contextlib import contextmanager
+
+# https://stackoverflow.com/a/41904558/35544
+@contextmanager
+def add_to_path(p):
+    import sys
+    old_path = sys.path
+    old_modules = sys.modules
+    sys.modules = old_modules.copy()
+    sys.path = sys.path[:]
+    sys.path.insert(0, p)
+    try:
+        yield
+    finally:
+        sys.path = old_path
+        sys.modules = old_modules
+
 
 module_name = "DynamicLoadedModule"
 
-spec = importlib.util.spec_from_file_location(module_name, input_file_path)
-dynamicModule = importlib.util.module_from_spec(spec)
-# We deliberately don't make visible via sys.modules[module_name] = module
-spec.loader.exec_module(dynamicModule)
+with add_to_path(input_directory):
+    spec = importlib.util.spec_from_file_location(module_name, Path(input_file_path))
+    dynamicModule = importlib.util.module_from_spec(spec)
+    # We deliberately don't make visible via sys.modules[module_name] = module
+    spec.loader.exec_module(dynamicModule)
 
 add_edef = "(edef addATEN (Vec (Vec Float)) ((Vec (Vec Float)) Float))"
 
