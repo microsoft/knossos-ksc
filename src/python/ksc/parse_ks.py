daf1f71e0e7e44690bcfd11b35dc93e3f78f497e
Andrew Fitzgibbon <awf@microsoft.com>
2020-10-21 14:05:11 +0100

    Comment

--- src/python/ksc/parse_ks.py
+++ src/python/ksc/parse_ks.py
@@ -180,7 +180,7 @@ def strip_block_comments(string):
     # Strip block comments
     regex = r"""\#\|               # hash bar
                     (
-                        [^\|\#]+    # Seq of (not bar or hash)
+                        [^\|\#]     # (not bar or hash)  Do not be tempted to suffix with "+" - bad perf
                     |               # or...
                         (\|(?!\#))  # (bar not followed by hash)
                     |               # or...
