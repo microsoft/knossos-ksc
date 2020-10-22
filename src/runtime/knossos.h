500cc3980db40ba9609e6fb0752e8a592df2cf47
David Collier <david.collier@microsoft.com>
2020-10-22 09:28:13 +0100

    Too much worrying about == vs !=

--- src/runtime/knossos.h
+++ src/runtime/knossos.h
@@ -506,7 +506,7 @@ namespace ks
 				return false;
 			}
 			for (int i = 0; i != size(); ++i) {
-				if (!((*this)[i] == other[i])) {
+				if ((*this)[i] != other[i]) {
 					return false;
 				}
 			}
