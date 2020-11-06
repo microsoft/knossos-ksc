4a772ceef6937f71d5d854598d8fcc725a30d6dc
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-11-06 14:38:51 +0000

    KATHash as in paper

--- src/Hash/KATHashFastOrigHash.hs
+++ src/Hash/KATHashFastOrigHash.hs
@@ -4,6 +4,7 @@ module KATHashFastOrigHash where
 
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import Data.Bits (xor)
 import Data.Hashable (Hashable, hash)
 import Data.List (foldl')
 
@@ -13,6 +14,8 @@ type Hash = Int
 type Structure = Hash
 type Positions = Hash
 type StructureTag = Structure
+
+data VarMap name = VM (Map name Positions) Hash
 
 mkSVar :: Structure
 mkSVar = hash "SVar"
@@ -33,8 +36,13 @@ mkSApp a b c d = hash (("SApp", 0 :: Int, a, 0 :: Int, b, 0 :: Int), 0 :: Int, (
 structureTag :: Structure -> StructureTag
 structureTag = id
 
-removeFromVM :: Ord v => v -> Map v positions -> (Map v positions, Maybe positions)
-removeFromVM v m = (Map.delete v m, Map.lookup v m)
+removeFromVM :: (Hashable v, Ord v) => v -> VarMap v -> (VarMap v, Maybe Positions)
+removeFromVM key vm@(VM entries existingHash)
+  | Just pt <- Map.lookup key entries
+  = (VM (key `Map.delete` entries) (existingHash `xor` entryHash key pt),
+     Map.lookup key entries)
+  | otherwise
+  = (vm, Nothing)
 
 findSingleton :: Map p Positions -> p
 findSingleton m = case Map.toList m of
@@ -45,20 +53,48 @@ findSingleton m = case Map.toList m of
 extendVM :: Ord k => Map k a -> k -> a -> Map k a
 extendVM m x p = Map.insert x p m
 
+entryHash :: Hashable name => name -> Positions -> Hash
+entryHash key pos = hash (key, 0 :: Int, pos, 0 :: Int)
+
+singletonVM :: Hashable name => name -> Positions -> VarMap name
+singletonVM key pos = VM (Map.singleton key pos) (entryHash key pos)
+
+alterVM :: (Hashable name, Ord name)
+        => (Maybe Positions -> Positions)
+        -> name -> VarMap name -> VarMap name
+alterVM f key (VM entries old_hash)
+  | Just old_pt <- Map.lookup key entries
+  , let new_pt = f (Just old_pt)
+  = VM (Map.insert key new_pt entries)
+       (old_hash `xor` entryHash key old_pt `xor` entryHash key new_pt)
+  | otherwise
+  , let new_pt = f Nothing
+  = VM (Map.insert key new_pt entries)
+       (old_hash `xor` entryHash key new_pt)
+
+hashVM :: VarMap name -> Hash
+hashVM (VM _ h) = h
+
+sizeVM :: VarMap name -> Int
+sizeVM (VM m _) = Map.size m
+
+toListVM :: VarMap name -> [(name, Positions)]
+toListVM (VM m _) = Map.toList m
+
 summariseExpr :: (Hashable name, Ord name)
               => Expr h name
-              -> (Structure, Map name Positions, Expr Hash name)
+              -> (Structure, VarMap name, Expr Hash name)
 summariseExpr = \case
   Var _ v   ->
     let structure   = mkSVar
-        positionMap = Map.singleton v mkHerePL
-    in (structure, positionMap, Var (hash (structure, 0 :: Int, Map.toList positionMap, 0 :: Int)) v)
+        positionMap = singletonVM v mkHerePL
+    in (structure, positionMap, Var (hash (structure, 0 :: Int, hashVM positionMap, 0 :: Int)) v)
   Lam _ x e ->
     let (str_body, map_body, e') = summariseExpr e
         (e_map, x_pos) = removeFromVM x map_body
         structure = mkSLam (structureTag str_body) x_pos str_body
         positionMap = e_map
-    in (structure, positionMap, Lam (hash (structure, 0 :: Int, Map.toList positionMap, 0 :: Int)) x e')
+    in (structure, positionMap, Lam (hash (structure, 0 :: Int, hashVM positionMap, 0 :: Int)) x e')
   App _ e1 e2 ->
     let (str1, map1, e1') = summariseExpr e1
         (str2, map2, e2') = summariseExpr e2
@@ -66,16 +102,16 @@ summariseExpr = \case
           hash (structureTag str1, 0 :: Int, structureTag str2, 0 :: Int)
         tag = app_depth
         str = mkSApp tag left_bigger str1 str2
-        vm = foldl' add_kv big_vm (Map.toList small_vm)
-        left_bigger = Map.size map1 >= Map.size map2
+        vm = foldl' add_kv big_vm (toListVM small_vm)
+        left_bigger = sizeVM map1 >= sizeVM map2
 
         (big_vm, small_vm) = if left_bigger
                              then (map1, map2)
                              else (map2, map1)
 
-        add_kv vm_ (v, p) = Map.alter (\mp -> Just (mkJoinPL tag mp p)) v vm_
+        add_kv vm_ (v, p) = alterVM (\mp -> mkJoinPL tag mp p) v vm_
 
-    in (str, vm, App (hash (str, 0 :: Int, Map.toList vm, 0 :: Int)) e1' e2')
+    in (str, vm, App (hash (str, 0 :: Int, hashVM vm, 0 :: Int)) e1' e2')
 
 katHash :: (Ord name, Hashable name) => Expr h name -> Expr Hash name
 katHash e = e'
