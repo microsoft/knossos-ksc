1cd26cc3215e96db1d401315f41ac04af2618371
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-11-10 14:45:07 +0000

    Preserve more structure

--- src/Hash/Main.hs
+++ src/Hash/Main.hs
@@ -9,19 +9,49 @@
 --    https://github.com/microsoft/knossos-ksc#compiling-the-ksc-executable
 
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveTraversable #-}
 
 module Main where
 
 import Benchmark
 import Expr (exprSize, Expr)
-import Hash (castHashOptimized, deBruijnHash, deBruijnNestedHash, naiveHashNested, Hash)
+import qualified Data.Foldable
 import qualified Hash
 
 import System.Environment (getArgs)
 
+data Algorithms a = Algorithms
+  { aNaiveNested    :: a
+  , aDeBrujin       :: a
+  , aDeBruijnNested :: a
+  , aCastHashOpt    :: a
+  }
+  deriving (Functor, Foldable, Traversable)
+
+data Expressions a = Expressions
+  { eMnistCNN :: a
+  , eGMMRev   :: a
+  , eGMM      :: a
+  }
+  deriving (Functor, Foldable, Traversable)
+
+algorithms :: Algorithms (Expr h String -> Expr Hash.Hash String)
+algorithms = Algorithms
+  { aNaiveNested    = Hash.naiveHashNested
+  , aDeBrujin       = Hash.deBruijnHash
+  , aDeBruijnNested = Hash.deBruijnNestedHash
+  , aCastHashOpt    = Hash.castHashOptimized
+  }
+
+expressions :: Expressions String
+expressions = Expressions
+  { eMnistCNN = "mnistcnn"
+  , eGMMRev   = "gmm-rev"
+  , eGMM      = "gmm"
+  }
 
 testcase_names :: [String]
-testcase_names = ["mnistcnn", "gmm-rev", "gmm"]
+testcase_names = Data.Foldable.toList expressions
 
 testcase_paths :: [FilePath]
 testcase_paths =
@@ -36,7 +66,7 @@ print_expr_sizes paths = do
   exprs <- traverse readExpr paths
   print (map exprSize exprs)
 
-print_stats_row :: (Expr () String -> Expr Hash String) -> IO ()
+print_stats_row :: (Expr () String -> Expr Hash.Hash String) -> IO ()
 print_stats_row algorithm = do
   result <- flip traverse testcase_paths (\t -> Benchmark.benchmarkOneReadFile t 50 50 (seqHashResult . algorithm))
   print (map process_stats result)
@@ -47,10 +77,7 @@ main = do
     ["manual"] -> do
       print testcase_names
       print_expr_sizes testcase_paths
-      print_stats_row naiveHashNested
-      print_stats_row deBruijnHash
-      print_stats_row deBruijnNestedHash
-      print_stats_row castHashOptimized
+      mapM_ print_stats_row algorithms
     ["random"] -> Benchmark.benchmark
     ["test"] -> Hash.testEverythingInFileStartingWith'prop_'
     _ -> putStrLn "Unsupported argument"
