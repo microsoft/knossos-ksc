628a795b5248af516f8b1bfc9c92f8c09632010e
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-11-17 18:38:29 +0000

    Split off ManualBenchmark

--- src/Hash/Main.hs
+++ src/Hash/Main.hs
@@ -9,65 +9,18 @@
 --    https://github.com/microsoft/knossos-ksc#compiling-the-ksc-executable
 
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE DeriveTraversable #-}
-
-module Main where
 
 import qualified Collision
 import qualified Benchmark
-import Expr (exprSize)
+import qualified ManualBenchmark
 import qualified Hash
 
-import qualified Data.Foldable
 import System.Environment (getArgs)
-
-data Expressions a = Expressions
-  { eMnistCNN :: a
-  , eGMMRev   :: a
-  , eGMM      :: a
-  , eBERTs    :: [a]
-  }
-  deriving (Functor, Foldable, Traversable)
-
-expressions :: Expressions String
-expressions = Expressions
-  { eMnistCNN = "mnistcnn"
-  , eGMMRev   = "gmm-rev"
-  , eGMM      = "gmm"
-  , eBERTs    = map (\i -> "bert/bert" ++ show i) [1 :: Int .. 23]
-  }
-
-testcase_path :: String -> FilePath
-testcase_path = \name -> "./exprs/" ++ name ++ ".expr"
-
-process_stats :: Benchmark.AggregateStatistics -> (Int, Int)
-process_stats aggregate_stats =
-  let (_, mean, _, _, stddev) = Benchmark.stats aggregate_stats in (round mean, round stddev)
-
-specific_benchmarks :: IO ()
-specific_benchmarks = do
-      loadedExpressions <- flip traverse expressions $ \name -> do
-        expr <- Benchmark.readExpr (testcase_path name)
-        pure (name, expr)
-
-      putStrLn "Expressions:"
-      flip Data.Foldable.traverse_ loadedExpressions $ \(name, expr) -> do
-        putStrLn (name ++ " (size " ++ show (exprSize expr) ++ ")")
-      putStrLn ""
-
-      flip Data.Foldable.traverse_ Benchmark.algorithms_ $ \(algorithmName, algorithm, _) -> do
-        putStrLn ("Algorithm " ++ algorithmName ++ ": ")
-
-        flip Data.Foldable.traverse_ loadedExpressions $ \(name, expr) -> do
-          putStr (name ++ ": ")
-          stats_ <- Benchmark.benchmarkOne 50 50 (Benchmark.seqHashResult . algorithm) expr
-          print (process_stats stats_)
-        putStrLn ""
 
 main :: IO ()
 main = do
   getArgs >>= \case
-    ["manual"] -> specific_benchmarks
+    ["manual"] -> ManualBenchmark.specific_benchmarks
     ["random", "fast"] -> Benchmark.benchmark Benchmark.fast
     ["random", "full"] -> Benchmark.benchmark Benchmark.full
     ["test"] -> Hash.testEverythingInFileStartingWith'prop_'
