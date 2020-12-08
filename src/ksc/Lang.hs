f856ba53819cea2eecee5da572558eb533160992
David Collier <david.collier@microsoft.com>
2020-12-08 19:42:56 +0000

    Type checking for multi-dimensional versions of primitives (#474)

--- src/ksc/Lang.hs
+++ src/ksc/Lang.hs
@@ -230,6 +230,37 @@ deriving instance Show (DeclX Parsed)
 deriving instance Show (RuleX Parsed)
 
 ----------------------------------
+--- Tensor properties
+----------------------------------
+
+{- Note [Size and index types of tensors]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Generally, tensor elements are indexed by tuples of integers,
+with the size of the tuple being the number of dimensions of
+the tensor. The same type (a tuple of integers) is used to
+represent the size of the tensor.
+
+The 1-dimensional case is special because we avoid the
+use of 1-tuples. In this case, the index/size type is a
+plain integer.
+-}
+
+tensorIndexType :: Int -> Type
+tensorIndexType 1 = TypeInteger
+tensorIndexType d = TypeTuple (replicate d TypeInteger)
+
+tensorDimensionFromIndexType :: Type -> Maybe Int
+tensorDimensionFromIndexType TypeInteger = Just 1
+tensorDimensionFromIndexType (TypeTuple ts)
+  | all (`eqType` TypeInteger) ts
+  = Just (length ts)
+tensorDimensionFromIndexType _ = Nothing
+
+tensorTypeFromIndexType :: Type -> Type -> Maybe Type
+tensorTypeFromIndexType indexType elementType =
+  fmap (\d -> TypeTensor d elementType) (tensorDimensionFromIndexType indexType)
+
+----------------------------------
 --- Tangent space
 
 tangentType :: HasCallStack => Type -> Type
