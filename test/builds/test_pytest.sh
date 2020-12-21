535eadbc0ada92fe02ecf2f99b76190417d32e0d
Andrew Fitzgibbon <awf@microsoft.com>
2020-12-21 16:40:43 +0000

    Add ts2k test to CI

--- test/builds/test_pytest.sh
+++ test/builds/test_pytest.sh
@@ -12,5 +12,8 @@ cd ../..
 echo Running pytest
 python3 -m pytest test/python
 
+echo Running pytest on ts2k
+python3 -m pytest test/ts2k
+
 echo Running pytest using cpp backend
 python3 -m pytest test/python/test_tracing_core.py --backend cpp
