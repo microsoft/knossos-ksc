1309bdd2069645eab37d6ca591c7b74819831061
Andrew Fitzgibbon <awf@microsoft.com>
2020-12-21 16:12:55 +0000

    Basics of Expr refactor

--- src/ts2k/ts2ks/ts2ks.py
+++ src/ts2k/ts2ks/ts2ks.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 import sexpdata
 import functools
 import torch
@@ -26,9 +28,6 @@ warnings.filterwarnings("always")
 # Newline constant for s-expr printing
 nl = "\n"
 tab = "\t"
-
-def to_sexp(ex):
-    return sexpdata.Symbol(pformat(ex)) # Hack to print it back into the sexpdata stream, while we move to Expr ctors
 
 # CallMethod resolution:
 # https://github.com/pytorch/pytorch/blob/b6bb644e41b3928b5a515330ad35c8b447fcb876/torch/csrc/jit/serialization/python_print.cpp#L984-L1004
@@ -77,11 +76,7 @@ def from_torch_type(t):
 def make_arg(input):
     input_type = from_torch_type(input.type())
     name = mangleDebugName(input.debugName())
-    return [
-        sexpdata.Symbol(name),
-        sexpdata.Symbol(":"),
-        to_sexp(input_type),
-    ]
+    return Var(name, input_type, decl=True)
 
 def make_constant(node):
 
@@ -98,112 +93,55 @@ def make_constant(node):
     except RuntimeError:
         literal = "FUNCTIONCALL"
 
-    return [sexpdata.Symbol("\n"), sexpdata.Symbol("_" + value.debugName()), literal]
+    return Var("_" + value.debugName()), Const(literal)
 
 
 def make_print(node):
     mangled_id = mangleDebugName(node.inputsAt(0).debugName())
-    return sexpdata.Symbol("print"), sexpdata.Symbol(mangled_id)
-
-
-def make_list_init_inner(values, i, rescue):
-    value = values[0]
-    rhs = [sexpdata.Symbol("assert"), sexpdata.Symbol("false"), sexpdata.Symbol(rescue)]
-    if len(values) > 1:
-        rhs = make_list_init_inner(values[1:], i + 1, rescue)
-    return [
-        sexpdata.Symbol("if"),
-        [sexpdata.Symbol("eq"), sexpdata.Symbol("ni"), i],
-        sexpdata.Symbol(mangleDebugName(value.debugName())),
-        rhs,
-    ]
-
-
-def make_list_init(values):
-    # We may switch to vector literals later: https://github.com/microsoft/knossos-ksc/issues/310
-    # in the meantime here's a quick-and-dirty translation to chained if
-    # CAUTION: if it goes out of range it uses the first value!
-    return make_list_init_inner(values, 0, mangleDebugName(values[0].debugName()))
+    return Var("print"), Var(mangled_id)
 
 
 def make_list(node):
     value = node.outputsAt(0)
-
-    list_size = sum(1 for _ in node.inputs())
-    if list_size == 0:
-        return (
-            []
-        )  # may not be very practical on the Knossos side, some of the TorchScript workarounds currently mutates in place
-    else:
-        return [
-            sexpdata.Symbol("\n"),
-            sexpdata.Symbol("_" + value.debugName()),
-            [
-                sexpdata.Symbol("build"),
-                sexpdata.Symbol(str(list_size)),
-                [
-                    sexpdata.Symbol("lam"),
-                    [
-                        sexpdata.Symbol("ni"),
-                        sexpdata.Symbol(":"),
-                        sexpdata.Symbol("Integer"),
-                    ],
-                    make_list_init(list(node.inputs())),
-                ],
-            ],
-        ]
+    return Var(mangleDebugName(value.debugName())), Call("vec", [Var(mangleDebugName(i.debugName())) for i in node.inputs()])
 
 
 def make_tensor(node):
     # tensors aren't explicitly modelled in Knossos yet, leave them as identity over a (jagged) list for now
     value = node.outputsAt(0)
 
-    return [
-        sexpdata.Symbol("\n"),
-        sexpdata.Symbol(mangleDebugName(value.debugName())),
-        sexpdata.Symbol(mangleDebugName(node.inputsAt(0).debugName())),
-    ]
+    return Var(mangleDebugName(value.debugName())), Var(mangleDebugName(node.inputsAt(0).debugName()))
 
 
 def make_aten_function(node, value, function_name):
-    return [
-        sexpdata.Symbol("\n"),
-        sexpdata.Symbol(mangleDebugName(value.debugName())),
-        [
-            sexpdata.Symbol(function_name),
-            *[sexpdata.Symbol(mangleDebugName(i.debugName())) for i in node.inputs()]
-        ],
-    ]
+    return Var(mangleDebugName(value.debugName())), Call(function_name, [Var(mangleDebugName(i.debugName())) for i in node.inputs()])
+
 
 def make_return(node):
     mangled_id = mangleDebugName(node.inputsAt(0).debugName())
-    return sexpdata.Symbol(mangled_id)
+    return Var(mangled_id)
 
 
 def make_callfunction(node):
     value = node.outputsAt(0)
     function_name_constant = node.inputsAt(0).node()
+    function_name = function_name_constant.s("name")
     input1 = node.inputsAt(
         1
     )  # TODO: get all inputs instead of just first, 0th is function name itself
-    function_name = function_name_constant.s("name")
-    return [
-        sexpdata.Symbol("\n"),
-        sexpdata.Symbol(mangleDebugName(value.debugName())),
-        [  # get a function call - brackets are escaped by sexpdata
-            sexpdata.Symbol(function_name),
-            sexpdata.Symbol(mangleDebugName(input1.debugName())),
-        ],
-    ]
+    assert len(list(node.inputs())) == 2
+    return Var(mangleDebugName(value.debugName())), Call(function_name, mangleDebugName(input1.debugName()))
 
+def make_lets(bindings, body) -> Expr:
+    for (v,rhs) in reversed(bindings):
+        body = Let(v, rhs, body)
+    return body
 
 def make_if(make_binds, node):
     def make_branch(block):
         binds = make_binds(block.nodes())
-        if len(binds) > 0:
-            return [sexpdata.Symbol("let")] + binds + [make_return(block.returnNode())]
-        else:
-            return make_return(block.returnNode())
+        body = make_return(block.returnNode())
+        return make_lets(binds, body)
 
     identifier = None
     if node.outputsSize() == 0:
@@ -227,23 +165,13 @@ def make_if(make_binds, node):
     success_branch = make_branch(blocks[0])
     failure_branch = make_branch(blocks[1])
 
-    return [
-        sexpdata.Symbol("\n"),
-        sexpdata.Symbol(identifier),
-        [
-            sexpdata.Symbol("if"),
-            sexpdata.Symbol(conditional),
-            sexpdata.Symbol("\n\t"),
-            success_branch,
-            sexpdata.Symbol("\n\t"),
-            failure_branch,
-            sexpdata.Symbol("\n"),
-        ],
-    ]
+    return Var(identifier), If(Var(conditional), 
+                               success_branch,
+                               failure_branch)
 
 def ts2ks_fromgraph(generate_edefs, name, graph):
 
-    def translate_node(make_binds, node):
+    def translate_node(make_binds, node) -> Tuple[Var, Expr]:
         lookups = {
             "prim::Constant": make_constant,
             "prim::Print": make_print,
@@ -261,10 +189,10 @@ def ts2ks_fromgraph(generate_edefs, name, graph):
             return make_aten_function(node, node.outputsAt(0), kind)
 
         print("WARNING, unimplmented node kind: " + node.kind())
-        return sexpdata.Symbol("")
+        return sexpdata.Symbol("ERR"), sexpdata.Symbol(node.kind()), 
 
 
-    def make_binds(nodes):
+    def make_binds(nodes) -> List[Tuple[Var, Expr]]:
         return [
             translate_node(make_binds, node)
             for node in nodes
@@ -301,19 +229,12 @@ def ts2ks_fromgraph(generate_edefs, name, graph):
         op = translate_node(make_binds, return_node)
         return_type = from_torch_type(return_node.inputsAt(0).type())
 
-    body = [
-        sexpdata.Symbol("\n"),
-        sexpdata.Symbol("let"),
-        binds,
-        sexpdata.Symbol("\n"),
-        op,
-    ]
+    print(binds)
+    body = make_lets(binds, op)
 
-    name_as_symbol = sexpdata.Symbol(name)
+    whole_exp = Def(name, return_type, args, body)
 
-    whole_exp = [sexpdata.Symbol("def"), name_as_symbol, to_sexp(return_type), args, body]
-
-    return sexpdata.dumps(whole_exp)
+    return pformat(whole_exp)
 
 
 # TODO: make an configuration named tuple rather than passing flags
