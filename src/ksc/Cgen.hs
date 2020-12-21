6247c4e59656f6d00afcbb385095864e46537b30
Andrew Fitzgibbon <awf@microsoft.com>
2020-12-21 13:54:47 +0000

    Mangle colon in C++

--- src/ksc/Cgen.hs
+++ src/ksc/Cgen.hs
@@ -548,6 +548,7 @@ mangleFun = substitute $ \case
     '<' -> Just "$d"
     '>' -> Just "$b"
     '*' -> Just "$x"
+    ':' -> Just "$8"
     _   -> Nothing
 
 -- | Produces a short string that uniquely identifies the input type.
