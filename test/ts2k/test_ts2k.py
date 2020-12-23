fd39765437ff919ae3559119c3809fde7aa7ffb0
Andrew Fitzgibbon <awf@microsoft.com>
2020-12-23 16:43:15 +0000

    Add hand-written gradient to test

--- test/ts2k/test_ts2k.py
+++ test/ts2k/test_ts2k.py
@@ -5,12 +5,24 @@ from ksc import utils
 from ksc.type import Type
 from ts2ks import ts2mod
 
-def bar(a : int, x : float, b : str):
+def bar1(a : int, x : float, b : str):
     if a < 0:
         t = -0.125*x
     else:
         t = 1/2 * x ** 2
     return torch.sin(t)*t
+
+def grad_bar1(a : int, x : float, b : str):
+    """
+    Hand-written gradient of bar1, used to test AD
+    """
+    if a < 0:
+        t = -0.125*x
+        dtdx = -0.125
+    else:
+        t = 1/2 * x ** 2
+        dtdx = x
+    return torch.sin(t) + t*torch.cos(t)
 
 def relux(x: float):
     if x < 0.0:
@@ -37,7 +49,7 @@ def compile_relux():
     global ks_relux
     if ks_relux is None:
         print("Compiling relux")
-        ks_relux = ts2mod(relux, [Type.Float], Type.Float)
+        ks_relux = ts2mod(relux)
 
 def test_ts2k_relux():
     compile_relux()
@@ -57,11 +69,30 @@ def bar(a : int, x : float):
         t = 1/2 * x * float(b)
     return math.sin(t)*t
 
+def grad_bar(a : int, x : float):
+    """
+    Hand-written gradient of bar, used to test AD
+    """
+    b = 2
+    if a < 0:
+        t = -0.125*x
+        dtdx = -0.125
+    else:
+        t = 1/2 * x * float(b)
+        dtdx = float(b)/2
+    dda = ()
+    ddx = math.sin(t) + t*math.cos(t)
+    return dda,ddx
+
 def test_bar():
-    ks_bar = ts2mod(bar, [Type.Integer, Type.Float], Type.Float)
-    ks_ans = ks_bar(1,12.34)
-    ans = bar(1,12.34)
+    ks_bar = ts2mod(bar)
+    a,x = 1,12.34
+    ks_ans = ks_bar(a,x)
+    ans = bar(a,x)
     assert ans == ks_ans
+
+    assert ks_bar.rev((a,x),1.0) == grad_bar(a,x)
+
 
 # def test_Vec_init():
 #     def f(x : float):
