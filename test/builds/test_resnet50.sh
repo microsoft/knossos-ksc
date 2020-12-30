61391288bc30e3bd7afc61b568e0d3118c6a1029
Andrew Fitzgibbon <awf@microsoft.com>
2020-12-30 10:37:27 +0000

    Stop putting generated files in the top level directory

--- test/builds/test_resnet50.sh
+++ test/builds/test_resnet50.sh
@@ -11,12 +11,15 @@ cd ./src/python
 python3 -m pip install --editable .
 cd ../..
 
+
+TMPDIR="obj/test"
+mkdir -p $TMPDIR
+
 echo Translating Knossos to Python
-python3 -m ksc.translate examples/dl-resnet/resnet_v2.ks --backend jax_input_last > resnet_v2.py
+python3 -m ksc.translate examples/dl-resnet/resnet_v2.ks --backend jax_input_last > $TMPDIR/resnet_v2.py
 
 echo Running test of Resnet50 written in Knossos
-PYTHONPATH=. python ./test/builds/test_resnet50.py --model resnet_v2
+PYTHONPATH=$TMPDIR python ./test/builds/test_resnet50.py --model resnet_v2
 
 echo Running test of Resnet50 written in Python
-PYTHONPATH=. python ./test/builds/test_resnet50.py --model resnet_py
-
+PYTHONPATH=$TMPDIR python ./test/builds/test_resnet50.py --model resnet_py
