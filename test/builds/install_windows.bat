2c3f477fe4b8e9658977311d119f0c43eb0c308c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-01-12 11:32:52 +0000

    fixup

--- test/builds/install_windows.bat
+++ test/builds/install_windows.bat
@@ -4,8 +4,8 @@ echo ----- test g++ installation -----
 g++ --version
 where g++
 echo ----- install ghc -----
-choco install cabal --allow-downgrade --version 3.0.0.0 -y || exit /b
 choco install ghc --allow-downgrade --version 8.6.5 -y || exit /b
+refreshenv && choco install cabal --allow-downgrade --version 3.0.0.0 -y || exit /b
 echo ----- cabal install -----
 refreshenv && cabal new-update && cabal install hspec parsec mtl hashable || exit /b
 echo ----- installation done -----
