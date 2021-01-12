4fbdb930714c62454660e6304878d1429bc1eb9b
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-01-12 11:14:08 +0000

    Bring Windows GHC and Cabal versions inline with Linux versions

--- test/builds/install_windows.bat
+++ test/builds/install_windows.bat
@@ -4,8 +4,8 @@ echo ----- test g++ installation -----
 g++ --version
 where g++
 echo ----- install ghc -----
-choco install cabal --allow-downgrade --version 2.4.1.0 -y || exit /b
-choco install ghc --allow-downgrade --version 8.4.4 -y || exit /b
+choco install cabal --allow-downgrade --version 3.0.0.0 -y || exit /b
+choco install ghc --allow-downgrade --version 8.6.5 -y || exit /b
 echo ----- cabal install -----
 refreshenv && cabal new-update && cabal install hspec parsec mtl hashable || exit /b
 echo ----- installation done -----
