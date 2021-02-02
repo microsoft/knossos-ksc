ad905039bd7f7eeb00e9ce1c680aa5601dbda42a
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-02-02 19:59:27 +0000

    Use general traversal instead of specific setter
    That is, baseUserFunType instead of userFunAddType

--- src/ksc/Ksc/Traversal.hs
+++ src/ksc/Ksc/Traversal.hs
@@ -1,8 +1,15 @@
 module Ksc.Traversal where
 
 import qualified Data.Traversable as T
+import qualified Data.Functor.Identity as I
 
 traverseState :: Traversable t
               => (a -> s -> (b, s)) -> t a -> s -> (t b, s)
 traverseState = swap_ . T.mapAccumL . swap_
   where swap_ f x0 x1 = let (y0, y1) = f x1 x0 in (y1, y0)
+
+mapOf :: ((a -> I.Identity b) -> (s -> I.Identity t))
+      -> (a -> b)
+      -> s
+      -> t
+mapOf fmap' f = I.runIdentity . fmap' (I.Identity . f)
