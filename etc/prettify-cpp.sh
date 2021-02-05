0ba6a3315cd21969762b3a6a9ab882a01bbfb6f3
Andrew Fitzgibbon <awf@microsoft.com>
2021-02-05 11:58:18 +0000

    Add C++ prettifier

---
+++ etc/prettify-cpp.sh
@@ -0,0 +1,8 @@
+# Make our output C++ a tiny bit prettier (possibly introducing name clashes)
+sed -r\
+ -e 's/aten\$8.8/A_/g'\
+ -e 's/\$alloc/alloc/g'\
+ -e 's/^([ \t]+)ty\$[a-zA-Z0-9_$]+/\1auto/g'\
+ -e 's/rev\$/Grad_/g'\
+ -e 's/\bc\$/c/g'\
+ -e 's/\$a[A-Za-z0-9_$]+//' $*
