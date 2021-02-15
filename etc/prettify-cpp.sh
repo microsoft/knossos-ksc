ce9269405fd427127e0a78cae20f318d2575f341
Andrew Fitzgibbon <awf@microsoft.com>
2021-02-15 17:35:36 +0000

    Address PR comments

--- etc/prettify-cpp.sh
+++ etc/prettify-cpp.sh
@@ -1,8 +1,10 @@
 # Make our output C++ a tiny bit prettier (possibly introducing name clashes)
 sed -r\
- -e 's/aten\$8.8/at::/g'\
  -e 's/\$alloc/alloc/g'\
  -e 's/^([ \t]+)ty\$[a-zA-Z0-9_$]+/\1auto/g'\
  -e 's/rev\$/Grad_/g'\
  -e 's/\bc\$/c/g'\
- -e 's/\$a[A-Za-z0-9_$]+//' $*
+ -e 's/\$a[A-Za-z0-9_$]+//g'\
+ -e 's/ks::alloc_mark_t (.*) = alloc->mark\(\)/ks::mark \1(alloc);/' \
+ -e 's/aten\$8.8/at::/g'\
+ $*
