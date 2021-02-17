49d36828e7c7daf8e173dae2d8c303fbffaa6800
Andrew Fitzgibbon <awf@microsoft.com>
2021-02-17 16:09:15 +0000

    Add relu3 example

---
+++ examples/dl-relu3/relu3.py
@@ -0,0 +1,23 @@
+
+def relu3(x : float) -> float:
+  """
+  relu3 -- Like relu, but smoother, with a robust backbone
+  """
+  if x < 0.0:
+    return 0.0
+  elif x < 1.0:
+    return 1/3 * x ** 3
+  else:
+    return x - 2/3
+
+
+def plot_relu3(filename):
+  import matplotlib.pyplot as plt
+  import numpy as np
+
+  t = np.arange(-3,3,step=0.1)
+
+  plt.plot(t, [relu3(t) for t in t])
+  plt.gca().set_aspect(1)
+  print(f"Saving figure to {filename}")
+  plt.savefig(filename)
