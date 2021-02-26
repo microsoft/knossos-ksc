6195bb828d9c66a290ef71d72df99a44688b47d3
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-02-26 08:16:31 +0000

    Remove unused mapAccumLOf

--- src/ksc/Ksc/Traversal.hs
+++ src/ksc/Ksc/Traversal.hs
@@ -1,7 +1,5 @@
 module Ksc.Traversal where
 
-import qualified Control.Monad.State as S
-import           Data.Tuple (swap)
 import qualified Data.Traversable as T
 import qualified Data.Functor.Identity as I
 
@@ -28,11 +26,3 @@ traverseOf :: Applicative f
            -> s
            -> f t
 traverseOf = id
-
--- From
--- https://www.stackage.org/haddock/lts-16.27/lens-4.18.1/Control-Lens-Combinators.html#v:mapAccumLOf,
--- but I don't want to pick up an entire lens dependency
-mapAccumLOf :: ((a -> S.State acc b) -> (s -> S.State acc t))
-            -> (acc -> a -> (acc, b)) -> acc -> s -> (acc, t)
-mapAccumLOf l f acc0 s = swap (S.runState (l g s) acc0) where
-     g a = S.state $ \acc -> swap (f acc a)
