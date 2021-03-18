4a14790b9328667f0b7564eb16fb820e8f17d99d
Colin Gravill <colin.gravill@microsoft.com>
2021-03-18 15:30:02 +0000

    Try running Python tests on Windows too

---
+++ test/builds/test_pytest.cmd
@@ -0,0 +1,24 @@
+set -e
+
+echo Installing dependencies...
+python -m pip install -r src/python/requirements.txt
+python -m pip install pytest numpy torch==1.8.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+
+echo Installing ksc...
+cd ./src/python
+python -m pip install --editable .
+cd ../..
+
+echo Running pytest
+python -m pytest test/python
+
+echo Installing TS2KS...
+cd ./src/ts2k
+python -m pip install --editable .
+cd ../..
+
+echo Running pytest on ts2k
+python -m pytest test/ts2k
+
+echo Running pytest using cpp backend
+python -m pytest test/python/test_tracing_core.py --backend cpp
