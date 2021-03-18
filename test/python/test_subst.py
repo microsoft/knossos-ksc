ea098ffbb83531afbc7c49deff0f7cea4c72e7a1
Alan Lawrence (MSRC) <allawr@microsoft.com>
2021-03-18 17:36:50 +0000

    Rewrite tests using parser

--- test/python/test_subst.py
+++ test/python/test_subst.py
@@ -1,31 +1,32 @@
 import pytest
 
-from ksc.expr import Var, Const, Let, Lam, Call, If, Assert
+from ksc.expr import Var, Const, Let, Lam, Call
 from ksc.cav_subst import replace_free_vars, replace_subtree, replace_subtrees, ReplaceLocationRequest, _make_nonfree_var
+from ksc.parse_ks import parse_expr_string
 
 def test_make_nonfree_var():
   assert _make_nonfree_var(Var("x")) == Var("_0")
   assert _make_nonfree_var(Var("_0")) == Var("_1")
   assert _make_nonfree_var(Var("_0x")) == Var("_0")
   assert _make_nonfree_var(Var("_1")) == Var("_0")
-  assert _make_nonfree_var(If(Var("_0"), Var("_3"), Var("x"))) in [Var("_1"), Var("_2"), Var("_4")]
+  assert _make_nonfree_var(parse_expr_string("(if _0 _3 x)")) in [Var("_1"), Var("_2"), Var("_4")]
 
 def test_replace_free_vars():
   # Replaces Var
-  e = Let(Var("x"), Call("add", [Var("y"), Const(1)]), Call("mul", [Var("x"), Const(2)]))
+  e = parse_expr_string("(let (x (add y 1)) (mul x 2))")
   replaced = replace_free_vars(e, {"y": Var("z")})
-  expected = Let(Var("x"), Call("add", [Var("z"), Const(1)]), Call("mul", [Var("x"), Const(2)]))
+  expected = parse_expr_string("(let (x (add z 1)) (mul x 2))")
   assert replaced == expected
 
   # Replaces target of Call
   replaced2 = replace_free_vars(e, {"add": Var("foo")})
-  expected2 = Let(Var("x"), Call("foo", [Var("y"), Const(1)]), Call("mul", [Var("x"), Const(2)]))
+  expected2 = parse_expr_string("(let (x (foo y 1)) (mul x 2))")
   assert replaced2 == expected2
 
 def test_replace_free_vars_shadowing():
-  e = Let(Var("x"), Call("add", [Var("x"), Const(1)]), Call("mul", [Var("x"), Const(2)]))
+  e = parse_expr_string("(let (x (add x 1)) (mul x 2))")
   replaced = replace_free_vars(e, {"x": Var("z")})
-  expected = Let(Var("x"), Call("add", [Var("z"), Const(1)]), Call("mul", [Var("x"), Const(2)]))
+  expected = parse_expr_string("(let (x (add z 1)) (mul x 2))")
   assert replaced == expected
 
 def _get_node(expr, idx):
@@ -39,16 +40,16 @@ def _get_node(expr, idx):
     idx -= ch.num_nodes
 
 def test_replace_subtrees():
-  e = Call("foo", [Var("x"), Var("y"), Var("z")])
+  e = parse_expr_string("(foo x y z)")
   replaced = replace_subtrees(e, [
     ReplaceLocationRequest(3, Var("w")),
     ReplaceLocationRequest(2, Var("v"))])
-  expected = Call("foo", [Var("x"), Var("v"), Var("w")])
+  expected = parse_expr_string("(foo x v w)")
   assert replaced == expected
 
 def test_replace_subtrees_nested():
-  e = Assert(Const(True), Call("foo", [Var("x"), Var("y"), Var("z")]))
-  inner_replaced = Call("foo", [Var("x"), Var("y"), Var("w")])
+  e = parse_expr_string("(assert true (foo x y z))")
+  inner_replaced = parse_expr_string("(foo x y w)")
   def replacer(e1, e2):
     assert e1 == Const(0)
     assert e2 == inner_replaced
@@ -62,50 +63,51 @@ def test_replace_subtrees_nested():
     ])
 
 def test_replace_subtree_avoids_capture():
-  e = Let(Var("x"), If(Var("p"), Var("a"), Var("b")), Call("add", [Var("x"), Var("y")]))
-  new_subtree = Call("mul", [Var("x"), Const(2)])
+  e = parse_expr_string("(let (x (if p a b)) (add x y))")
+  new_subtree = parse_expr_string("(mul x 2)")
   assert _get_node(e, 7) == Var("y")
   replaced = replace_subtree(e, 7, new_subtree)
   # Must rename the "x".
   new_var = _make_nonfree_var(e) # No alpha-equivalence, so this is the name used.
-  expected = Let(new_var, If(Var("p"), Var("a"), Var("b")), Call("add", [new_var, new_subtree]))
+  expected = parse_expr_string(f"(let ({new_var} (if p a b)) (add {new_var} (mul x 2)))")
   assert replaced == expected
 
 def test_replace_subtree_avoids_capturing_another():
-  e = Let(Var("x"), If(Var("p"), Var("a"), Var("b")), Call("foo", [Var("_0"), Var("x"), Var("y")]))
-  new_subtree = Call("mul", [Var("x"), Const(2)])
+  new_subtree = parse_expr_string("(mul x 2)")
+  conflicting_var = _make_nonfree_var(new_subtree)  # But, this already exists
+  e = parse_expr_string(f"(let (x (if p a b)) (foo {conflicting_var} x y))")
   assert _get_node(e, 8) == Var("y")
   replaced = replace_subtree(e, 8, new_subtree)
-  # Must rename the "x".
-  new_var = _make_nonfree_var(e) # No alpha-equivalence, so this is the name used.
-  expected = Let(new_var, If(Var("p"), Var("a"), Var("b")), Call("foo", [Var("_0"), new_var, new_subtree]))
-  assert new_var != Var("_0")
+  new_var = replaced.vars  # No alpha-equivalence, so this is the name used.
+  expected = parse_expr_string(f"(let ({new_var} (if p a b)) (foo {conflicting_var} {new_var} (mul x 2)))")
+  assert new_var != conflicting_var
   assert replaced == expected
 
 def test_replace_subtree_applicator_allows_capture():
-  e = Let(Var("x"), If(Var("p"), Var("a"), Var("b")), Call("add", [Var("x"), Var("y")]))
-  new_subtree = Call("mul", [Var("x"), Const(2)])
+  e = parse_expr_string("(let (x (if p a b)) (add x y))")
+  new_subtree = parse_expr_string("(mul x 2)")
   assert _get_node(e, 7) == Var("y")
-  replaced = replace_subtree(e, 7, Const(0), lambda e1,e2: new_subtree)
-  expected = Let(Var("x"), If(Var("p"), Var("a"), Var("b")), Call("add", [Var("x"), new_subtree]))
+  replaced = replace_subtree(e, 7, Const(0), lambda _e1, _e2: new_subtree)
+  expected = parse_expr_string("(let (x (if p a b)) (add x (mul x 2)))")
   assert replaced == expected
 
 def test_replace_subtree_allows_inlining_call():
   # In the future this will become part of the inline-call rule,
   # but for now we merely test that replace_subtree supports it.
-  e = Let(Var("x"), If(Var("p"), Var("a"), Var("b")), Call("foo", [Var("x")]))
-  foo_impl = Lam(Var("foo_arg"), Call("add", [Var("x"), Var("foo_arg")]))
+  e = parse_expr_string("(let (x (if p a b)) (foo x))")
+  foo_impl = parse_expr_string("(lam (foo_arg : Float) (add x foo_arg))")
   assert type(_get_node(e, 5)) == Call
   def apply_to_argument(func, call):
     assert type(func) == Lam and type(call) == Call
     assert len(call.args) == 1  # More would require making a Tuple
-    return Let(func.arg, call.args[0], func.body)
+    return Let(Var(func.arg.name), # drop the type
+              call.args[0],
+              func.body)
   replaced = replace_subtree(e, 5, foo_impl, apply_to_argument)
   new_var = _make_nonfree_var(e)
-  expected = Let(new_var, # Bound variable renamed to avoid capturing "x" in foo_impl
-    If(Var("p"), Var("a"), Var("b")),
-    # The call:
-    Let(Var("foo_arg"), new_var, # Renaming applied to argument
-      Call("add", [Var("x"), Var("foo_arg")]) # Renaming not applied to the "x" in foo_impl
-  ))
+  expected = parse_expr_string(f"""(let ({new_var} (if p a b))  ; Bound variable renamed to avoid capturing "x" in foo_impl
+    ; The call:
+    (let (foo_arg {new_var}) ; Renaming applied to argument
+      (add x foo_arg))) ; Renaming not applied to the "x" in foo_impl
+   """)
   assert replaced == expected
