f7eff69e29562adaee06486dfdc9cbe3548b2882
Colin Gravill <colin.gravill@microsoft.com>
2021-03-22 14:05:50 +0000

    Make failable actions exit

--- test/builds/test_pytest_PyTorchPreview.cmd
+++ test/builds/test_pytest_PyTorchPreview.cmd
@@ -1,33 +1,33 @@
 set -e
 
 echo Set Pip to specific version...
-python -m pip install --force-reinstall pip==20.3
+python -m pip install --force-reinstall pip==20.3 || exit /b
 
 REM New resolver with PyTorch version notation https://pip.pypa.io/en/latest/user_guide/#changes-to-the-pip-dependency-resolver-in-20-3-2020
 REM Too big, throw memory exception https://stackoverflow.com/a/31526029/35544
 
 REM currently have backend issues on PyTorch 1.8.0 https://github.com/microsoft/knossos-ksc/issues/659 so trying nightly
 echo Installing dependencies...
-python -m pip install -r src/python/requirements.txt
-python -m pip install --pre --use-deprecated=legacy-resolver --no-cache-dir pytest numpy torch -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
+python -m pip install -r src/python/requirements.txt || exit /b
+python -m pip install --pre --use-deprecated=legacy-resolver --no-cache-dir pytest numpy torch -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html || exit /b
 
 echo Installing ksc...
 cd ./src/python
-python -m pip install --editable .
+python -m pip install --editable . || exit /b
 cd ../..
 
 echo Running pytest
-python -m pytest test/python
+python -m pytest test/python || exit /b
 
 echo Installing TS2KS...
 cd ./src/ts2k
-python -m pip install --editable .
+python -m pip install --editable . || exit /b
 cd ../..
 
 
 
 REM echo Running pytest on ts2k
-python -m pytest test/ts2k
+python -m pytest test/ts2k || exit /b
 
 REM echo Running pytest using cpp backend
 REM python -m pytest test/python/test_tracing_core.py --backend cpp
