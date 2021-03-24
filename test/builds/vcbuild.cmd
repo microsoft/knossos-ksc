b22194947a315cb41221f3072e033e3f53564d0b
Colin Gravill <1099725+cgravill@users.noreply.github.com>
2021-03-24 11:00:46 +0000

    Update vcbuild.cmd

--- test/builds/vcbuild.cmd
+++ test/builds/vcbuild.cmd
@@ -1,3 +1,4 @@
+@REM from https://github.com/microsoft/vswhere/wiki/Find-VC
 @echo off
 
 for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
