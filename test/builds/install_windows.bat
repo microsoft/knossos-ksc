5f32322b87862027e3234d497bf7b983d7c033d0
Colin Gravill <1099725+cgravill@users.noreply.github.com>
2021-03-29 12:11:05 +0100

    TorchScript to Knossos test with preview PyTorch on GitHub Actions (#669)
    * Try using PyTorch Preview for a nightly build - helps us detect if breakages occur early
    * Make failable actions exit
    * Create vcbuild.cmd
    https://github.com/microsoft/vswhere/wiki/Find-VC
    #679

--- test/builds/install_windows.bat
+++ test/builds/install_windows.bat
@@ -3,11 +3,11 @@ choco install mingw --version 8.1.0 -y || exit /b
 echo ----- test g++ installation -----
 g++ --version
 where g++
+echo ----- install Ninja -----
+choco install ninja || exit /b
 echo ----- install ghc -----
 choco install cabal --allow-downgrade --version 3.0.0.0 -y || exit /b
 choco install ghc --allow-downgrade --version 8.4.4 -y || exit /b
 echo ----- cabal install -----
-refreshenv && C:/ProgramData/chocolatey/lib/cabal/tools/cabal-3.0.0.0/cabal new-update || exit /b
-echo ----- install Ninja -----
-choco install ninja
+call refreshenv && C:/ProgramData/chocolatey/lib/cabal/tools/cabal-3.0.0.0/cabal new-update || exit /b
 echo ----- installation done -----
