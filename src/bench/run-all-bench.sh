174d26f46abf9950cc3f293fddb7c858bf8b4574
Andrew Fitzgibbon <awf@microsoft.com>
2021-04-06 10:10:12 +0100

    Disable relu3 waiting for elementwise_apply

--- src/bench/run-all-bench.sh
+++ src/bench/run-all-bench.sh
@@ -1,5 +1,5 @@
 # TODO: this should be a makefile.  fred.csv: fred.py etc
 BENCH=src/bench/run-bench.py
 
-PYTHONPATH=examples/dl-activations python $BENCH relu3 vrelu3 
+#PYTHONPATH=examples/dl-activations python $BENCH relu3 vrelu3 
 PYTHONPATH=examples/dl-capsule python $BENCH sqrl sqrl
