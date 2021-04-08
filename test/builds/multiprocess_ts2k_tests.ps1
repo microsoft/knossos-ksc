90706ae30af3cbd28ce5a4b86e6c2fcee5358fc5
Colin Gravill <colin.gravill@microsoft.com>
2021-04-08 14:58:03 +0100

    Show test list

--- test/builds/multiprocess_ts2k_tests.ps1
+++ test/builds/multiprocess_ts2k_tests.ps1
@@ -1,6 +1,9 @@
 # The tests run out of memory on CI see https://github.com/microsoft/knossos-ksc/issues/679 for details of the leak in module creation
 # pytest-forked on Windows doesn't help, so: collect the tests, run one at a time, exit on any failure
 $alltests = pytest $PSScriptRoot\..\ts2k\ --collect-only --quiet --disable-pytest-warnings | Select-Object -skip 2 -last 1000000
+
+$alltests
+
 Foreach ($test in $alltests)
 {
     $test
