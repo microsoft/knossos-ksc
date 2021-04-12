e5f156aa3e5d6a733e4256636ee1d06a27f8e676
Alan Lawrence (MSRC) <allawr@microsoft.com>
2021-04-12 23:06:40 +0100

    (More) Tests passing

--- src/python/ksc/rewrites.py
+++ src/python/ksc/rewrites.py
@@ -1,10 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import singledispatch
 from typing import Any, Iterator, Mapping, Tuple
 
-from abc import ABC, abstractmethod, abstractproperty
-from ksc.expr import Expr, Let, Lam, Var
-from ksc.cav_subst import Location
+from abc import ABC, abstractmethod
+from ksc.expr import Expr, Let, Lam, Var, Const, Call
+from ksc.cav_subst import Location, get_children, replace_subtree
 from ksc.utils import singleton
 
 @dataclass(frozen=True)
@@ -12,9 +12,11 @@ class Rewrite:
     rule: "Rule"
     root: Expr # RLO required rule to be passed to Rewrite.apply(), but this seems prone to accidents
     path: Location
-    extra_data: Mapping[str, Any]
 
-    def __call__():
+    # Anything the Rule needs to pass from matching to rewriting. Used immutably, but dataclasses don't allow default {}
+    extra_data: Mapping[str, Any] = field(default_factory=dict)
+
+    def __call__(self):
         return self.rule.apply_at(self.root, self.path, **self.extra_data)
     
 class AbstractMatcher(ABC):
@@ -43,7 +45,7 @@ class Rule(AbstractMatcher):
     def name(self):
         return self._name
 
-    @abstractproperty
+    @abstractmethod
     def possible_expr_classes(self):
         """ Return a set of the subclasses of Expr that this rule could possibly match. """
         pass
@@ -57,15 +59,15 @@ class Rule(AbstractMatcher):
         return (rule, (self.name,))
 
 class RuleSet(AbstractMatcher):
-    def init(self, rules):
+    def __init__(self, rules):
         # TODO also allow global (any-class) rules?
         self.rules_by_class = {}
         for rule in rules:
-            for clazz in rule.possible_expr_classes:
+            for clazz in rule.possible_expr_classes():
                 self.rules_by_class.setdefault(clazz, []).append(rule)
     
     def get_local_rewrites(self, subtree: Expr, path_from_root: Location, root: Expr, env: Mapping) -> Iterator[Rewrite]:
-        for rule in self.rules_by_class(subtree.__class__):
+        for rule in self.rules_by_class.get(subtree.__class__, []):
             yield from rule.get_local_rewrites(subtree, path_from_root, root, env)
 
 def _subtree_rewrites(e: Expr, path_from_root: Location, rules: AbstractMatcher, root: Expr, env: Mapping[str, Tuple[Location, Expr]]) -> Iterator[Rewrite]:
@@ -83,9 +85,11 @@ def _subtree_env_let(e: Let, loc: Location, rules: AbstractMatcher, root: Expr, 
     yield from _subtree_rewrites(e.rhs, loc + [0], rules, root, env)
     def is_tuple(e):
         return isinstance(e, Call) and e.name.mangle_without_type() == "tuple"
-    child_env = ({**env, e.var: (loc, e.rhs)} if isinstance(e.vars, Var)
-                 else {**env, **{var: (loc, elem) for var,elem in zip(e.vars, e.rhs.args)}} if is_tuple(e.rhs) # Individual tuple elements
-                 else {k:v for k,v in env.items() if k not in e.vars}) # Not individually inlinable
+    def _rm_dict_keys(d, ks):
+        return {k:v for k, v in d.items() if k not in ks}
+    child_env = ({**env, e.vars.name: (loc, e.rhs)} if isinstance(e.vars, Var)
+                 else {**env, **{var.name: (loc, elem) for var,elem in zip(e.vars, e.rhs.args)}} if is_tuple(e.rhs) # Individual tuple elements
+                 else _rm_dict_keys(env, [v.name for v in e.vars])) # Not individually inlinable
     yield from _subtree_rewrites(e.body, loc+[1], rules, root, child_env)
 
 @_subtree_update_env.register
@@ -95,7 +99,6 @@ def _subtree_env_lam(e: Lam, loc: Location, rules: AbstractMatcher, root: Expr, 
 
 @singleton
 class inline_var(Rule):
-    @property
     @staticmethod
     def possible_expr_classes():
         return {Var}
@@ -111,20 +114,19 @@ class inline_var(Rule):
         #        the first traversal building the environment (varname -> binding_location) as it goes down to the usage site - duplicating some of the work done by the matcher (which at the least needs to build an environment of which variables are inlinable)
         #        the second traversal uses two nested calls of replace_subtree: (a) from root down to the binder, without any renaming; then (b) from the binder down to the usage site, renaming to avoid capture of anything in the value-to-be-inlined.
         assert path_to_var[:len(binding_location)] == binding_location
-        return expr.replace_subtree(binding_location, Const(0.0), lambda _payload, let:
-            lambda _payload, let: let.replace_subtree(path_to_var[len(binding_location):], bound_value) # No applicator: just take bound_value
+        return replace_subtree(expr, binding_location, Const(0.0),
+            lambda _payload, let: replace_subtree(let, path_to_var[len(binding_location):], bound_value) # No applicator: just take bound_value
         )
 
     def get_local_rewrites(self, subtree: Expr, path_from_root: Location, root: Expr, env: Mapping[str, Tuple[Location, Expr]]) -> Iterator[Rewrite]:
         if isinstance(subtree, Var): # May not be if rule is used directly outside of a RuleSet. Q: Do we want to support that case.
             if subtree.name in env:
                 binding_loc, bound_val = env[subtree.name]
-                yield Rewrite(self, root, path_from_root, binding_location=binding_loc, bound_value=bound_val)
+                yield Rewrite(self, root, path_from_root, {"binding_location": binding_loc, "bound_value": bound_val})
 
 
 @singleton
 class delete_let(Rule):
-    @property
     @staticmethod
     def possible_expr_classes():
         return {Let}
@@ -132,19 +134,20 @@ class delete_let(Rule):
     @staticmethod
     def _let_vars_used(expr: Let) -> bool:
         if isinstance(expr.vars, Var):
-            return expr.vars in expr.body.free_vars
-        return any(v in expr.body.free_vars for v in expr.vars)
+            return expr.vars.name in expr.body.free_vars_
+        return any(v.name in expr.body.free_vars_ for v in expr.vars)
 
 
     def apply_at(self, expr: Expr, path: Location) -> Expr:
         # The constant here says that there are no free variables to avoid capturing en route to the target Location
-        return expr.replace_subtree(path, Const(0.0), self._apply)
+        return replace_subtree(expr, path, Const(0.0), self._apply)
 
     def _apply(self, const_zero: Expr, let_node: Expr) -> Expr:
         assert const_zero == Const(0.0) # Specified in apply_at
         assert not self._let_vars_used(let_node)
         return let_node.body
 
-    def get_local_rewrites(self, subtree: Expr, path_from_root: Location, root: Expr) -> Iterator[Rewrite]:
+    def get_local_rewrites(self, subtree: Expr, path_from_root: Location, root: Expr, env) -> Iterator[Rewrite]:
+        del env
         if isinstance(subtree, Let) and not self._let_vars_used(subtree):
             yield Rewrite(self, root, path_from_root)
--- test/python/test_rewrites.py
+++ test/python/test_rewrites.py
@@ -1,5 +1,5 @@
-from ksc.rewrites import rule, RuleSet
-from ksc.parse_ks import parse_expr
+from ksc.rewrites import rule, RuleSet, inline_var, delete_let
+from ksc.parse_ks import parse_expr_string
 
 def apply_in_only_location(rule_name, expr):
     cands = list(rule(rule_name).get_all_rewrites(expr))
@@ -10,30 +10,73 @@ def check_nowhere_applicable(rule_name, expr):
     assert len(list(rule(rule_name).get_all_rewrites(expr))) == 0
 
 def test_inline_var_single():
-    e = parse_expr("(let (a (div 1.0 x)) (div a (add a 1.0)))")
+    e = parse_expr_string("(let (a (div 1.0 x)) (div a (add a 1.0)))")
     # Should be exactly two candidates
-    rw_div, rw_add = sorted(
-        rule("inline_var").get_all_rewrites(e),
-        key=lambda rw: tuple(rw.path) # TODO drop tuple following merge
-    )
-    assert (rw_div.rule, rw_div.path) == ("inline_var", [1,0])
-    assert rw_div() == parse_expr("(let (a (div 1.0 x)) (div (div 1.0 x) (add a 1.0)))")
-    assert (rw_add.rule, rw_add.path) == ("inline_var", [1,1,0])
-    assert rw_add() == parse_expr("(let (a (div 1.0 x)) (div a (add (div 1.0 x) 1.0)))")
+    rw_div, rw_add = sorted(rule("inline_var").get_all_rewrites(e), key=lambda rw: tuple(rw.path))
+    assert (rw_div.rule, rw_div.path) == (inline_var(), [1,0])
+    assert rw_div() == parse_expr_string("(let (a (div 1.0 x)) (div (div 1.0 x) (add a 1.0)))")
+    assert (rw_add.rule, rw_add.path) == (inline_var(), [1,1,0])
+    assert rw_add() == parse_expr_string("(let (a (div 1.0 x)) (div a (add (div 1.0 x) 1.0)))")
 
     assert apply_in_only_location("inline_var", rw_div()
      ) ==  apply_in_only_location("inline_var", rw_add()
-     ) ==  parse_expr("(let (a (div 1.0 x)) (div (div 1.0 x) (add (div 1.0 x) 1.0)))")
-
-def test_inline_var_shadowing():
-    e = parse_expr("(add a (let (a 2) a)")
-    e2 = apply_in_only_location("inline_var", e)
-    assert e2 == parse_expr("(add a (let a 2) 2)")
-    check_nowhere_applicable("inline_var", e2)
+     ) ==  parse_expr_string("(let (a (div 1.0 x)) (div (div 1.0 x) (add (div 1.0 x) 1.0)))")
 
 def test_delete_let_single():
-    check_nowhere_applicable("delete_let", parse_expr("(let (a (div 1.0 x)) (div a (add a 1.0)))"))
-    check_nowhere_applicable("delete_let", parse_expr("(let (a (div 1.0 x)) (div (div 1.0 x) (add a 1.0)))"))
-    check_nowhere_applicable("delete_let", parse_expr("(let (a (div 1.0 x)) (div a (add (div 1.0 x)) 1.0)))"))
-    assert apply_in_only_location("delete_let", parse_expr("(let (a (div 1.0 x)) (div (div 1.0 x) (add (div 1.0 x)) 1.0)))")
-      ) == parse_expr("(div (div 1.0 x) (add (div 1.0 x)) 1.0)")
+    check_nowhere_applicable("delete_let", parse_expr_string("(let (a (div 1.0 x)) (div a (add a 1.0)))"))
+    check_nowhere_applicable("delete_let", parse_expr_string("(let (a (div 1.0 x)) (div (div 1.0 x) (add a 1.0)))"))
+    check_nowhere_applicable("delete_let", parse_expr_string("(let (a (div 1.0 x)) (div a (add (div 1.0 x) 1.0)))"))
+    assert apply_in_only_location("delete_let", parse_expr_string("(let (a (div 1.0 x)) (div (div 1.0 x) (add (div 1.0 x)) 1.0))")
+      ) == parse_expr_string("(div (div 1.0 x) (add (div 1.0 x)) 1.0)")
+
+def test_ruleset():
+    r = RuleSet([rule("inline_var"), rule("delete_let")])
+    e = parse_expr_string("(let (a (div 1.0 x)) (div a (add a 1.0)))")
+    # Should be exactly two candidates
+    rw_div, rw_add = sorted(r.get_all_rewrites(e), key=lambda rw: tuple(rw.path))
+    assert (rw_div.rule, rw_div.path) == (inline_var(), [1,0])
+    assert rw_div() == parse_expr_string("(let (a (div 1.0 x)) (div (div 1.0 x) (add a 1.0)))")
+    assert (rw_add.rule, rw_add.path) == (inline_var(), [1,1,0])
+    assert rw_add() == parse_expr_string("(let (a (div 1.0 x)) (div a (add (div 1.0 x) 1.0)))")
+
+    all_inlined = parse_expr_string("(let (a (div 1.0 x)) (div (div 1.0 x) (add (div 1.0 x) 1.0)))")
+    assert [rw() for rw in r.get_all_rewrites(rw_div())] == [rw() for rw in r.get_all_rewrites(rw_add())] == [
+        all_inlined
+    ]
+
+    # Now should be only one possible rewrite
+    rw_del, = list(r.get_all_rewrites(all_inlined))
+    assert (rw_del.rule, rw_del.path) == (delete_let(), [])
+    assert rw_del() == parse_expr_string("(div (div 1.0 x) (add (div 1.0 x) 1.0))")
+
+def sorted_rewrites(rule, expr):
+    return [rw() for rw in sorted(
+        rule.get_all_rewrites(expr), key=lambda rw: tuple(rw.path) # TODO drop tuple following merge
+    )]
+
+def test_inline_var_shadowing():
+    e = parse_expr_string("(add a (let (a 2) a))")
+    e2 = apply_in_only_location("inline_var", e)
+    assert e2 == parse_expr_string("(add a (let (a 2) 2))")
+    check_nowhere_applicable("inline_var", e2)
+
+def test_inline_var_rebinding():
+    e = parse_expr_string("(let (a 2) (add a (let (a 3) a)))")
+    assert sorted_rewrites(rule("inline_var"), e) == [
+        parse_expr_string("(let (a 2) (add 2 (let (a 3) a)))"),
+        parse_expr_string("(let (a 2) (add a (let (a 3) 3)))")
+    ]
+
+def test_inline_var_renames():
+    e = parse_expr_string("(let (a (add x 3)) (let (x 2) (add a x)))")
+    assert sorted_rewrites(rule("inline_var"), e) == [
+        # Must rename x. The test depends on the new name being picked as x_0.
+        parse_expr_string("(let (a (add x 3)) (let (x_0 2) (add (add x 3) x_0)))"),
+        # Can also just inline the x, no renaming required
+        parse_expr_string("(let (a (add x 3)) (let (x 2) (add a 2)))")
+    ]
+
+    e = parse_expr_string("(let (x (add x 1)) (add x 2))")
+    assert apply_in_only_location("inline_var", e) == parse_expr_string(
+        "(let (x_0 (add x 1)) (add (add x 1) 2))"
+    )
