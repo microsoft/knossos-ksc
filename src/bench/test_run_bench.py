ec1907ed430c42b84d75c5b43ea679c366984177
Colin Gravill <colin@gravill.com>
2021-04-16 16:39:56 +0100

    Add demonstration of pytest-benchmark For discussion, need to look at parameterization

---
+++ src/bench/test_run_bench.py
@@ -0,0 +1,17 @@
+import pytest
+
+# @pytest.mark.parametrize
+def test_benchmark_sqrl(benchmark):
+    from sqrl import sqrl_pt, sqrl_bench_configs
+
+    # can't call benchmark multiple times, need to use parameterization
+    # https://github.com/ionelmc/pytest-benchmark#examples
+    #for arg in sqrl_bench_configs():
+    #    result = benchmark(sqrl_pt, x=arg)
+
+    config = list(sqrl_bench_configs())
+
+    result = benchmark(sqrl_pt, x=config[0])
+
+    # random values so more involved to test results
+    #assert result == 0.00125
