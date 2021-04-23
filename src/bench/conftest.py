5c30322d0280a77a6712f9bdf1742bfe2fe39d2f
Colin Gravill <colin@gravill.com>
2021-04-23 15:15:18 +0100

    Customise names for clarity

--- src/bench/conftest.py
+++ src/bench/conftest.py
@@ -2,6 +2,7 @@
 import pytest
 import importlib
 import inspect
+from collections import namedtuple
 
 from ts2ks import ts2mod
 
@@ -21,21 +22,29 @@ def benchmarkname(request):
 def modulename(request):
     return request.config.getoption("--modulename")
 
+BenchmarkFunction = namedtuple('BenchmarkFunction', 'name func')
+
 def functions_to_benchmark(mod, benchmark_name, example_input):
     for fn in inspect.getmembers(mod, lambda m: inspect.isfunction(m) and m.__name__.startswith(benchmark_name)):
         fn_name, fn_obj = fn
         if fn_name == benchmark_name + '_bench_configs':
-            pass
+            continue
         elif fn_name == benchmark_name + '_pytorch':
-            yield fn_obj
+            yield BenchmarkFunction("PyTorch", fn_obj)
         elif fn_name == benchmark_name + '_pytorch_nice':
-            yield fn_obj
+            yield BenchmarkFunction("PyTorch Nice", fn_obj)
         elif fn_name == benchmark_name:
-            yield ts2mod(fn_obj, example_input).apply
+            yield BenchmarkFunction("Knossos", ts2mod(fn_obj, example_input).apply)
         else:
             # perhaps we should just allow anything that matches the pattern?
             # would make it easier to add arbitrary comparisons e.g. TF
             print(f"Ignoring {fn_name}")
+
+def func_namer(benchmark_func):
+    return benchmark_func.name
+
+def config_namer(config):
+    return str(config.shape)
 
 def pytest_generate_tests(metafunc):
     if "func" in metafunc.fixturenames:
@@ -49,13 +58,13 @@ def pytest_generate_tests(metafunc):
 
         example_inputs = (configs[0],)
 
-        metafunc.parametrize("func", list(functions_to_benchmark(mod, benchmark_name, example_inputs)))
+        metafunc.parametrize("func", functions_to_benchmark(mod, benchmark_name, example_inputs), ids=func_namer)
 
         # We want to group by tensor size, it's not clear how to metaprogram the group mark cleanly.
         # pytest meta programming conflates arguments and decoration. I've not been able to find a way to directly
         # parameterize just marks so do the mark along with a oarameter
         config_and_group_marker = [pytest.param(config, marks=[pytest.mark.benchmark(group=str(config.shape))]) for config in configs]
-        metafunc.parametrize("config", config_and_group_marker)
+        metafunc.parametrize("config", config_and_group_marker, ids=config_namer)
 
         # Alternative use a dummy argument --benchmark-group-by=param:dummy_argument but messes with serialised versions and is just horrible
 
--- src/bench/test_run_bench.py
+++ src/bench/test_run_bench.py
@@ -2,4 +2,4 @@ import pytest
 
 def test_benchmark(benchmark, func, config):
     # any assertion we can make about result?
-    result = benchmark(func, config)
+    result = benchmark(func.func, config)
