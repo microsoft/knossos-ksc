cb9159edbc00749b8766647383522d59de21a7b3
Alan Lawrence (MSRC) <allawr@microsoft.com>
2021-05-06 11:13:08 +0100

    Sort imports; rename spec=>to_lift; rm always-overridden apply_to_parent

--- src/python/ksc/rewrites.py
+++ src/python/ksc/rewrites.py
@@ -7,8 +7,8 @@ from typing import Any, FrozenSet, Iterator, Optional, List, Mapping, Tuple, Uni
 from pyrsistent import pmap
 from pyrsistent.typing import PMap
 
-from ksc.cav_subst import Location, VariableSubstitution, get_children, replace_subtree, replace_free_vars, make_nonfree_var, get_node_at_location
-from ksc.expr import ConstantType, StructuredName, Expr, Let, Lam, Var, Const, Call, If, Rule
+from ksc.cav_subst import Location, VariableSubstitution, get_children, get_node_at_location, make_nonfree_var, replace_free_vars, replace_subtree
+from ksc.expr import ConstantType, StructuredName, Call, Const, Expr, If, Let, Lam, Rule, Var
 from ksc.parse_ks import parse_ks_file, parse_ks_string
 from ksc.type import Type
 from ksc.type_propagate import type_propagate
@@ -375,7 +375,10 @@ def parse_rules_from_file(filename):
 #   lift_if: (foo (if p x y)) ==> (if p (foo x) (foo y))
 # where foo can be any variety of Expr.
 
-def can_speculate_ahead_of_condition(e: Expr, cond: Expr) -> bool:
+def can_speculate_ahead_of_condition(e: Expr, cond: Expr, cond_value: bool) -> bool:
+    """ Can we speculatively evaluate e, when the original program would not have done so,
+        moving <e> to a point that it is evaluated before testing <cond> ?
+        <cond_value> indicates the value that <cond> would have to take in order for <e> to have been evaluated. """
     # TODO: check if 'e' might raise an exception if evaluated without testing 'cond' first
     return True
 
@@ -385,21 +388,21 @@ class LiftingRule(RuleMatcher):
     @abstractmethod
     def get_liftable_part(self, e: Expr) -> Optional[Expr]:
         """ If e can be lifted, return the part of 'e' that will be evaluated earlier; otherwise None """
-    
+
     def matches_for_possible_expr(self, subtree: Expr, path_from_root: Location, root: Expr, env: LetBindingEnvironment) -> Iterator[Match]:
         if isinstance(subtree, Lam):
             # Lam's inside build/sumbuild are handled as part of the (sum)build
             return
         for i, ch in enumerate(get_children(subtree)):
             nested_lam = isinstance(ch, Lam)
-            spec = self.get_liftable_part(ch.body if nested_lam else ch)
-            if spec is None:
-                continue
-            if isinstance(subtree, Let) and i==1 and subtree.vars.name in spec.free_vars_:
-                pass # Cannot lift computation outside of let that involves the free variable
-            elif nested_lam and ch.arg.name in spec.free_vars_:
-                pass # Cannot lift loop-variant computation out of lam within (sum)build
-            elif isinstance(subtree, If) and i > 0 and not can_speculate_ahead_of_condition(ch, subtree.cond):
+            to_lift = self.get_liftable_part(ch.body if nested_lam else ch)
+            if to_lift is None:
+                pass
+            elif isinstance(subtree, Let) and i==1 and subtree.vars.name in to_lift.free_vars_:
+                pass # Cannot lift computation using a variable outside of let that binds that variable
+            elif nested_lam and ch.arg.name in to_lift.free_vars_:
+                pass # Similarly - cannot lift loop-variant computation out of lam within (sum)build
+            elif isinstance(subtree, If) and i > 0 and not can_speculate_ahead_of_condition(to_lift, subtree.cond, i==1):
                 pass # Don't lift computation out of "if" that may be guarding against an exception
             else:
                 yield Match(self, root, path_from_root + ((i,0) if nested_lam else (i,)), {"buildlam": nested_lam})
@@ -410,11 +413,13 @@ class LiftingRule(RuleMatcher):
         path_to_parent = path[:-len(rest_of_path)]
         return replace_subtree(e, path_to_parent, Const(0.0), lambda _, parent: self.apply_to_parent(parent, rest_of_path))
 
+    @abstractmethod
     def apply_to_parent(self, parent: Expr, path_to_child: Location) -> Expr:
-        child_to_lift = get_node(parent, rest_of_path)
-        return self.build_lifted(child_to_lift, 
-            lambda new_child: replace_subtree(parent, rest_of_path, new_child))
-    
+        """ Args -
+            <parent>: the node to lift over
+            <path_to_child> path within parent identifying the liftable child (maybe a grandchild to lift over (sum)build+lam).
+        Return an Expr equivalent to <parent>. """
+
 @singleton
 class lift_if(LiftingRule):
     def get_liftable_part(self, e: Expr):
