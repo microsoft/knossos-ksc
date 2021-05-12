425855070b6fffd0ec53ddcf1e76fe6b3a094555
Colin Gravill <colin@gravill.com>
2021-05-12 09:06:31 +0100

    Backwards path

--- src/bench/test_run_bench.py
+++ src/bench/test_run_bench.py
@@ -1,6 +1,21 @@
 import pytest
+import torch
+import time
 
 
-def test_benchmark(benchmark, func, config):
+def test_forward(benchmark, func, config):
     # any assertion we can make about result?
     result = benchmark(func.func, config)
+
+
+def test_backwards(benchmark, func, config):
+
+    def create_fresh_args():
+        config.requires_grad = True
+        loss = func.func(config).sum()
+        return (loss, config), {}
+
+    # We need to use pedantic mode to have a fresh arguments via a setup function,
+    # but that means we need to specify rounds ourselves.
+    # TODO: Investigate more 
+    result = benchmark.pedantic(torch.autograd.grad, setup=create_fresh_args, rounds=500)
