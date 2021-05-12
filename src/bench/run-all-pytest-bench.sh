4f5d698504e6b8925817874f0f5208dadb2f286d
Colin Gravill <colin@gravill.com>
2021-05-12 10:05:28 +0100

    Add sorting & group for relu3 commented

--- src/bench/run-all-pytest-bench.sh
+++ src/bench/run-all-pytest-bench.sh
@@ -1,5 +1,5 @@
 # TODO: this should be a makefile.  fred.csv: fred.py etc
 # BENCH=src/bench/run-bench.py
 
-#PYTHONPATH=examples/dl-activations pytest src/bench/ --modulename=relu3 --benchmarkname=vrelu3 --benchmark-autosave
+#PYTHONPATH=examples/dl-activations pytest src/bench/ --benchmark-sort=name --benchmark-group-by=group,func --modulename=relu3 --benchmarkname=vrelu3 --benchmark-autosave
 PYTHONPATH=examples/dl-capsule pytest src/bench/ --benchmark-sort=name --benchmark-group-by=group,func --modulename=sqrl --benchmarkname=sqrl --benchmark-autosave
