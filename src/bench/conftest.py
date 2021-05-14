3a3acdb73335e0fe24c6866d95ef8ac863cf5f32
Colin Gravill <colin@gravill.com>
2021-05-14 13:37:02 +0100

    Move config to function called once

--- src/bench/conftest.py
+++ src/bench/conftest.py
@@ -56,40 +56,46 @@ def config_namer(config):
     return str(config.shape)
 
 
+def pytest_configure(config):
+    module_name = config.getoption("modulename")
+    benchmark_name = config.getoption("benchmarkname")
+
+    mod = importlib.import_module(module_name)
+
+    configs = list(getattr(mod, benchmark_name + "_bench_configs")())
+
+    example_inputs = (configs[0],)
+
+    config.reference_func = getattr(mod, benchmark_name + "_pytorch")
+    config.functions_to_benchmark = list(
+        functions_to_benchmark(mod, benchmark_name, example_inputs)
+    )
+    # We want to group by tensor size, it's not clear how to metaprogram the group mark cleanly.
+    # pytest meta programming conflates arguments and decoration. I've not been able to find a way to directly
+    # parameterize just marks so do the mark along with a oarameter
+    config.config_and_group_marker = [
+        pytest.param(config, marks=[pytest.mark.benchmark(group=str(config.shape))])
+        for config in configs
+    ]
+
+    # Alternative use a dummy argument --benchmark-group-by=param:dummy_argument but messes with serialised versions and is just horrible
+
+
 def pytest_generate_tests(metafunc):
+    print("pytest_generate_tests")
     if "func" in metafunc.fixturenames:
-
-        module_name = metafunc.config.getoption("modulename")
-        benchmark_name = metafunc.config.getoption("benchmarkname")
-
-        mod = importlib.import_module(module_name)
-
-        configs = list(getattr(mod, benchmark_name + "_bench_configs")())
-
-        example_inputs = (configs[0],)
-
-        reference_func = getattr(mod, benchmark_name + "_pytorch")
-
+        config = metafunc.config
         metafunc.parametrize(
-            "reference_func", [reference_func],
+            "reference_func", [config.reference_func],
         )
 
         metafunc.parametrize(
-            "func",
-            functions_to_benchmark(mod, benchmark_name, example_inputs),
-            ids=func_namer,
+            "func", config.functions_to_benchmark, ids=func_namer,
         )
 
-        # We want to group by tensor size, it's not clear how to metaprogram the group mark cleanly.
-        # pytest meta programming conflates arguments and decoration. I've not been able to find a way to directly
-        # parameterize just marks so do the mark along with a oarameter
-        config_and_group_marker = [
-            pytest.param(config, marks=[pytest.mark.benchmark(group=str(config.shape))])
-            for config in configs
-        ]
-        metafunc.parametrize("config", config_and_group_marker, ids=config_namer)
+        metafunc.parametrize("config", config.config_and_group_marker, ids=config_namer)
 
-        # Alternative use a dummy argument --benchmark-group-by=param:dummy_argument but messes with serialised versions and is just horrible
+        
 
         # https://github.com/pytest-dev/pytest/issues/1425#issuecomment-439220834
         # This runs, but doesn't affect the test. It seems like they've allowed FunctionDefinition to be mutable (due to bad inheritance)
