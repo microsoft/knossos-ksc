4f89b15f1e282c6261abf31c24f8eec89b564ec0
Alan Lawrence (MSRC) <allawr@microsoft.com>
2021-05-18 21:19:15 +0100

    Move fixture to conftest.py, imports

--- test/python/test_rewrites.py
+++ test/python/test_rewrites.py
@@ -1,18 +1,10 @@
 import pytest
 from ksc.alpha_equiv import are_alpha_equivalent
 from ksc.rewrites import rule, RuleSet, inline_var, delete_let, parse_rule_str
-from ksc.parse_ks import parse_expr_string, parse_ks_file, parse_ks_filename
+from ksc.parse_ks import parse_expr_string
 from ksc.type import Type
 from ksc.type_propagate import type_propagate_decls
 from ksc import utils
-
-
-@pytest.fixture
-def prelude_symtab():
-    symtab = {}
-    decls_prelude = list(parse_ks_filename("src/runtime/prelude.ks"))
-    type_propagate_decls(decls_prelude, symtab)
-    return symtab
 
 
 def apply_in_only_location(rule_name, expr):
