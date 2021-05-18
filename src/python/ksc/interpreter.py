857f04f5d2b89e535601cd0c7d493b448008236f
Alan Lawrence (MSRC) <allawr@microsoft.com>
2021-05-18 20:25:31 +0100

    Remove _mksn, black

--- src/python/ksc/interpreter.py
+++ src/python/ksc/interpreter.py
@@ -9,26 +9,46 @@ from ksc.type import Type
 # with no free variables. TODO: investigate combining with abstract_value.py and backends/ directory.
 
 
-def _mksn(s: str, t: Type) -> StructuredName:
-    return make_structured_name((s, t))
-
-
 native_impls: Mapping[StructuredName, Callable] = {
-    _mksn("add", Type.Tuple(Type.Integer, Type.Integer)): lambda a, b: a + b,
-    _mksn("add", Type.Tuple(Type.Float, Type.Float)): lambda a, b: a + b,
-    _mksn("sub", Type.Tuple(Type.Integer, Type.Integer)): lambda a, b: a - b,
-    _mksn("sub", Type.Tuple(Type.Float, Type.Float)): lambda a, b: a - b,
-    _mksn("mul", Type.Tuple(Type.Integer, Type.Integer)): lambda a, b: a * b,
-    _mksn("mul", Type.Tuple(Type.Float, Type.Float)): lambda a, b: a * b,
-    _mksn("div", Type.Tuple(Type.Integer, Type.Integer)): lambda a, b: a // b,
-    _mksn("div", Type.Tuple(Type.Float, Type.Float)): lambda a, b: a / b,
-    _mksn("exp", Type.Float): lambda a: numpy.exp(a),
-    _mksn("log", Type.Float): lambda a: numpy.log(a),
-    _mksn("eq", Type.Tuple(Type.Integer, Type.Integer)): lambda a, b: a == b,
-    _mksn("eq", Type.Tuple(Type.Float, Type.Float)): lambda a, b: a == b,
-    _mksn("gt", Type.Tuple(Type.Integer, Type.Integer)): lambda a, b: a > b,
-    _mksn("gt", Type.Tuple(Type.Float, Type.Float)): lambda a, b: a > b,
-    _mksn("gte", Type.Tuple(Type.Integer, Type.Integer)): lambda a, b: a >= b,
-    _mksn("gte", Type.Tuple(Type.Float, Type.Float)): lambda a, b: a >= b,
-    _mksn("to_float", Type.Integer): float,
+    make_structured_name(
+        ("add", Type.Tuple(Type.Integer, Type.Integer))
+    ): lambda a, b: a
+    + b,
+    make_structured_name(("add", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a
+    + b,
+    make_structured_name(
+        ("sub", Type.Tuple(Type.Integer, Type.Integer))
+    ): lambda a, b: a
+    - b,
+    make_structured_name(("sub", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a
+    - b,
+    make_structured_name(
+        ("mul", Type.Tuple(Type.Integer, Type.Integer))
+    ): lambda a, b: a
+    * b,
+    make_structured_name(("mul", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a
+    * b,
+    make_structured_name(
+        ("div", Type.Tuple(Type.Integer, Type.Integer))
+    ): lambda a, b: a
+    // b,
+    make_structured_name(("div", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a
+    / b,
+    make_structured_name(("exp", Type.Float)): lambda a: numpy.exp(a),
+    make_structured_name(("log", Type.Float)): lambda a: numpy.log(a),
+    make_structured_name(("eq", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a
+    == b,
+    make_structured_name(("eq", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a
+    == b,
+    make_structured_name(("gt", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a
+    > b,
+    make_structured_name(("gt", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a
+    > b,
+    make_structured_name(
+        ("gte", Type.Tuple(Type.Integer, Type.Integer))
+    ): lambda a, b: a
+    >= b,
+    make_structured_name(("gte", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a
+    >= b,
+    make_structured_name(("to_float", Type.Integer)): float,
 }
