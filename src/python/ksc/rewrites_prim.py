607eccb8fea0ae66466784ba2afa90a1c6200d9b
Alan Lawrence (MSRC) <allawr@microsoft.com>
2021-05-19 10:57:48 +0100

    black

--- src/python/ksc/rewrites_prim.py
+++ src/python/ksc/rewrites_prim.py
@@ -1,6 +1,9 @@
 from ksc.rewrites import parse_rule_str
 
-index_of_build = parse_rule_str("""(rule "index_of_build"
+index_of_build = parse_rule_str(
+    """(rule "index_of_build"
     ((idx : Integer) (n : Integer) (e : Any))
     (index idx (build n (lam (i : Integer) e)))
-    (let (i idx) e))""", {})
+    (let (i idx) e))""",
+    {},
+)
