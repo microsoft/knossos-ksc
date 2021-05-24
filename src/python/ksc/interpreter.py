4cfdcd1566f3356682c46344a90ac895dd84dfa6
Alan Lawrence (MSRC) <allawr@microsoft.com>
2021-05-24 19:05:08 +0100

    Use make_structured_name and "operator"

--- src/python/ksc/interpreter.py
+++ src/python/ksc/interpreter.py
@@ -1,3 +1,4 @@
+from operator import add, iadd, sub, mul, ifloordiv, truediv, eq, gt, ge
 import numpy
 from typing import Callable, Mapping
 
@@ -8,24 +9,22 @@ from ksc.type import Type
 # Largely a placeholder for an interpreter, which in the future may be capable of interpreting any Expr
 # with no free variables. TODO: investigate combining with abstract_value.py and backends/ directory.
 
-mksn = make_structured_name
-
 native_impls: Mapping[StructuredName, Callable] = {
-    mksn(("add", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a + b,
-    mksn(("add", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a + b,
-    mksn(("sub", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a - b,
-    mksn(("sub", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a - b,
-    mksn(("mul", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a * b,
-    mksn(("mul", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a * b,
-    mksn(("div", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a // b,
-    mksn(("div", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a / b,
-    mksn(("exp", Type.Float)): lambda a: numpy.exp(a),
-    mksn(("log", Type.Float)): lambda a: numpy.log(a),
-    mksn(("eq", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a == b,
-    mksn(("eq", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a == b,
-    mksn(("gt", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a > b,
-    mksn(("gt", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a > b,
-    mksn(("gte", Type.Tuple(Type.Integer, Type.Integer))): lambda a, b: a >= b,
-    mksn(("gte", Type.Tuple(Type.Float, Type.Float))): lambda a, b: a >= b,
-    mksn(("to_float", Type.Integer)): float,
+    make_structured_name(("add", Type.Tuple(Type.Integer, Type.Integer))): iadd,
+    make_structured_name(("add", Type.Tuple(Type.Float, Type.Float))): add,
+    make_structured_name(("sub", Type.Tuple(Type.Integer, Type.Integer))): sub,
+    make_structured_name(("sub", Type.Tuple(Type.Float, Type.Float))): sub,
+    make_structured_name(("mul", Type.Tuple(Type.Integer, Type.Integer))): mul,
+    make_structured_name(("mul", Type.Tuple(Type.Float, Type.Float))): mul,
+    make_structured_name(("div", Type.Tuple(Type.Integer, Type.Integer))): ifloordiv,
+    make_structured_name(("div", Type.Tuple(Type.Float, Type.Float))): truediv,
+    make_structured_name(("exp", Type.Float)): numpy.exp,
+    make_structured_name(("log", Type.Float)): numpy.log,
+    make_structured_name(("eq", Type.Tuple(Type.Integer, Type.Integer))): eq,
+    make_structured_name(("eq", Type.Tuple(Type.Float, Type.Float))): eq,
+    make_structured_name(("gt", Type.Tuple(Type.Integer, Type.Integer))): gt,
+    make_structured_name(("gt", Type.Tuple(Type.Float, Type.Float))): gt,
+    make_structured_name(("gte", Type.Tuple(Type.Integer, Type.Integer))): gt,
+    make_structured_name(("gte", Type.Tuple(Type.Float, Type.Float))): ge,
+    make_structured_name(("to_float", Type.Integer)): float,
 }
