8a4fa821542bec32a43c59139da75dda82a172e6
Colin Gravill <colin.gravill@microsoft.com>
2021-06-15 17:40:15 +0200

    More types Scaling

--- src/bench/comparison_plot.py
+++ src/bench/comparison_plot.py
@@ -1,15 +1,30 @@
-import os
+from dataclasses import dataclass
 import pytest_benchmark
 import pytest_benchmark.storage
 import pytest_benchmark.storage.file
 import pytest_benchmark.utils
 import dateutil.parser
 import datetime
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from shutil import copyfile
 
+
 storage = pytest_benchmark.utils.load_storage(".benchmarks", logger=None, netrc=None)
+
+
+@dataclass(frozen=True)
+class FigureBundle:
+    figure: Figure
+    axis: plt.axes
+
+
+@dataclass(frozen=True)
+class FigureLookup:
+    method: str
+    configuration: str
+
 
 groupedbenchmarks = defaultdict(list)
 for path, content in storage.load():
@@ -39,13 +54,13 @@ def labelkey(name):
 
 
 def make_figure():
-    fig = plt.figure()
-    axis = fig.add_subplot(111)
+    figure = plt.figure()
+    axis = figure.add_subplot(111)
 
     axis.tick_params(axis="x", rotation=50)
     axis.set_xlabel("Commit time")
-    axis.set_ylabel("median (seconds)")
-    return (fig, axis)
+    axis.set_ylabel("median (microseconds)")
+    return FigureBundle(figure=figure, axis=axis)
 
 
 figures = defaultdict(make_figure)
@@ -54,19 +69,25 @@ for test in ("test_forward", "test_backwards", "test_inference"):
     for time, benchmark in groupedbenchmarks[test]:
         group_name = benchmark["group"]
 
-        _, axis = figures[(test, group_name)]
+        axis = figures[FigureLookup(method=test, configuration=group_name)].axis
         method = benchmark["name"].split("-")[1]  # TODO: harden, use extra_info?
 
+        # TODO: generalise to more than sqrl
         axis.set_title(f"sqrl {test} {group_name}")
-        axis.plot(time, benchmark["stats"]["median"], labelkey(method), label=method)
+        axis.plot(
+            time, (benchmark["stats"]["median"] * 1000), labelkey(method), label=method
+        )
 
-for (test, group_name), (figure, axis) in figures.items():
+for figure_lookup, figure_bundle in figures.items():
     handles, labels = axis.get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
     axis.legend(by_label.values(), by_label.keys())
 
-    figure.savefig(
-        f"build/sqrl_{test}_{group_name}.svg".replace(" ", "_"), bbox_inches="tight"
+    filename = f"build/sqrl_{figure_lookup.method}_{figure_lookup.configuration}.svg".replace(
+        " ", "_"
     )
+    print(f"saving {filename}")
+
+    figure_bundle.figure.savefig(filename, bbox_inches="tight")
 
 copyfile("src/bench/sqrl.html", "build/sqrl.html")
