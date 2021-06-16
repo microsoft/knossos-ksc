eee0917647d09fec1ac407f33a6d490aaf5bfdf2
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-06-16 19:15:24 +0100

    Add gelu to run-all-pytest-bench.sh

--- src/bench/run-all-pytest-bench.sh
+++ src/bench/run-all-pytest-bench.sh
@@ -3,3 +3,4 @@
 
 #pytest src/bench/ --benchmark-sort=name --benchmark-group-by=group,func --modulepath=examples/dl-activations/relu3 --benchmarkname=vrelu3 --benchmark-autosave
 pytest src/bench/ --benchmark-sort=name --benchmark-group-by=group,func --modulepath=examples/dl-capsule/sqrl --benchmarkname=sqrl --benchmark-autosave
+pytest src/bench/ --benchmark-sort=name --benchmark-group-by=group,func --modulepath=examples/dl-activations/gelu --benchmarkname=vgelu --benchmark-autosave
