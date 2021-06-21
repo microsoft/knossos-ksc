3cd4e05ba6f06bffec2f5acbda70ea56b43215e5
Colin Gravill <colin@gravill.com>
2021-06-21 10:28:21 +0200

    Tweak naming display and columns

--- src/bench/run-all-pytest-bench.sh
+++ src/bench/run-all-pytest-bench.sh
@@ -1,5 +1,5 @@
 # TODO: this should be a makefile.  fred.csv: fred.py etc
-BENCH="pytest src/bench/ --benchmark-sort=name --benchmark-group-by=group,func --benchmark-autosave"
+BENCH="pytest src/bench/ --benchmark-name=short --benchmark-sort=name --benchmark-group-by=group,func --benchmark-autosave --benchmark-columns=median,mean,stddev,min,max,iqr,outliers,iterations,rounds"
 
 # $BENCH --modulepath=examples/dl-activations/relu3 --benchmarkname=vrelu3
 $BENCH --modulepath=examples/dl-capsule/sqrl --benchmarkname=sqrl
