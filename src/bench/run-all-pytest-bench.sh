b40a7a0078048dcd5ca23a5bc7a0adeb5cf2ce77
Colin Gravill <colin@gravill.com>
2021-06-21 16:09:42 +0200

    @awf new order

--- src/bench/run-all-pytest-bench.sh
+++ src/bench/run-all-pytest-bench.sh
@@ -1,5 +1,5 @@
 # TODO: this should be a makefile.  fred.csv: fred.py etc
-BENCH="pytest src/bench/ --benchmark-name=short --benchmark-sort=name --benchmark-group-by=group,func --benchmark-autosave --benchmark-columns=median,mean,stddev,min,max,iqr,outliers,iterations,rounds"
+BENCH="pytest src/bench/ --benchmark-name=short --benchmark-sort=name --benchmark-group-by=group,func --benchmark-autosave --benchmark-columns=median,iqr,outliers,mean,stddev,min,max,iterations,rounds"
 
 # $BENCH --modulepath=examples/dl-activations/relu3 --benchmarkname=vrelu3
 $BENCH --modulepath=examples/dl-capsule/sqrl --benchmarkname=sqrl
