9c2da3c151d0f734ba54884f0656198d2ff4eba2
Colin Gravill <colin.gravill@microsoft.com>
2021-06-22 14:33:51 +0200

    Add permalink to original

--- src/bench/benchmark_shim.py
+++ src/bench/benchmark_shim.py
@@ -5,10 +5,11 @@ import sys
 # we want an in-between, where we can give a setup function
 # but allow "rounds" to be automatically determined
 # TODO: potentially propose adding an optional setup function to the simple benchmark() function,
-# but it's messy as you can't pass args AND setup
+# but it's messy as you can't pass args AND setup.
 def benchmark_semi_pedantic(benchmark, function_to_benchmark, setup):
     # using internal functions to match pytest-benchmark.
     # we might want to just go our own direction in terms of logic
+    # https://github.com/ionelmc/pytest-benchmark/blob/996dbe519b5bcc9b103ea0e4aeb232c58b71fc8c/src/pytest_benchmark/fixture.py#L147-L154
     runner_args, runner_kwargs = setup()
     runner = benchmark._make_runner(
         function_to_benchmark, args=runner_args, kwargs=runner_kwargs
