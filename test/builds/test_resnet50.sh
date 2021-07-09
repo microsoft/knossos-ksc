a15958709a00b375c9d0e02b63e067eae01d9dbd
Andrew Fitzgibbon <awf@microsoft.com>
2021-07-09 14:38:48 +0000

    Reduce pip installing

--- test/builds/test_resnet50.sh
+++ test/builds/test_resnet50.sh
@@ -6,11 +6,7 @@ echo Installing dependencies...
 
 python3 -m pip install -U Pillow jax==0.1.57 jaxlib==0.1.37
 
-echo Installing ksc...
-cd ./src/python
-python3 -m pip install --editable .
-cd ../..
-
+export PYTHONPATH="./src/python"
 
 TMPDIR="obj/test"
 mkdir -p $TMPDIR
@@ -18,8 +14,10 @@ mkdir -p $TMPDIR
 echo Translating Knossos to Python
 python3 -m ksc.translate examples/dl-resnet/resnet_v2.ks --backend jax_input_last > $TMPDIR/resnet_v2.py
 
+PYTHONPATH="$PYTHONPATH":$TMPDIR 
+
 echo Running test of Resnet50 written in Knossos
-PYTHONPATH=$TMPDIR python ./test/builds/test_resnet50.py --model resnet_v2
+python ./test/builds/test_resnet50.py --model resnet_v2
 
 echo Running test of Resnet50 written in Python
-PYTHONPATH=$TMPDIR python ./test/builds/test_resnet50.py --model resnet_py
+python ./test/builds/test_resnet50.py --model resnet_py
