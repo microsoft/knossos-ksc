de49f4d36bacb68a788075d7b2e19004a09f5440
Andrew Fitzgibbon <awf@microsoft.com>
2021-07-09 14:38:07 +0000

    Remove pip install

--- .github/workflows/benchmark.sh
+++ .github/workflows/benchmark.sh
@@ -1,9 +1,4 @@
 echo Installing dependencies...
 python3 -m pip install -r src/bench/requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
 
-echo Installing ksc...
-cd ./src/python
-python3 -m pip install --editable .
-cd ../..
-
 sh ./src/bench/run-all-pytest-bench.sh
