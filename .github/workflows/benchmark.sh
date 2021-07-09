28455f65ff8683d507872f25cae6ccf66852e9c6
Andrew Fitzgibbon <awf@microsoft.com>
2021-07-09 15:40:36 +0000

    Add python requirements

--- .github/workflows/benchmark.sh
+++ .github/workflows/benchmark.sh
@@ -1,4 +1,7 @@
 echo Installing dependencies...
-python3 -m pip install -r src/bench/requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+python3 -m pip install \
+    -r src/python/requirements.txt \
+    -r src/bench/requirements.txt \
+    -f https://download.pytorch.org/whl/torch_stable.html
 
 sh ./src/bench/run-all-pytest-bench.sh
