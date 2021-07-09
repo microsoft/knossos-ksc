f64f9261171b2f0f7a8642f0e175dde579ba75f1
Andrew Fitzgibbon <awf@microsoft.com>
2021-07-09 18:03:15 +0100

    Add conftest.py (#929)
    Add pytest.ini, pointing to test and src/bench directories
    Add test/conftest.py, and use to set sys.path to src/python
    Remove pip install -e src/python in several places
    
    Upshot: can run testing on two knossos checkouts on the same machine

--- .github/workflows/benchmark.sh
+++ .github/workflows/benchmark.sh
@@ -1,9 +1,7 @@
 echo Installing dependencies...
-python3 -m pip install -r src/bench/requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
-
-echo Installing ksc...
-cd ./src/python
-python3 -m pip install --editable .
-cd ../..
+python3 -m pip install \
+    -r src/python/requirements.txt \
+    -r src/bench/requirements.txt \
+    -f https://download.pytorch.org/whl/torch_stable.html
 
 sh ./src/bench/run-all-pytest-bench.sh
