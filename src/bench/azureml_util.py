389c2b0f2181248e17942ca3855c923c2bc74e80
Colin Gravill <colin.gravill@microsoft.com>
2021-07-28 08:27:11 +0100

    Gets src and examples onto AzureML Triggers benchmark before failing

--- src/bench/azureml_util.py
+++ src/bench/azureml_util.py
@@ -6,22 +6,24 @@ from tempfile import TemporaryDirectory
 
 # extracted and adapted from RLO
 
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+
+def copy_dir(target_dir, dir_name):
+    shutil.copytree(
+        os.path.join(root_dir, dir_name),
+        os.path.join(target_dir, dir_name),
+        ignore=shutil.ignore_patterns("__pycache__"),
+        dirs_exist_ok=True,  # tmpdir exists already
+    )
+
 
 @contextmanager
 def combined_source_directory():
-    root_dir = os.path.dirname(os.path.dirname(__file__))
+
     with TemporaryDirectory() as tmpdir:
         print("Copying sources to staging directory", tmpdir)
-        # This puts the contents of src directly into tmpdir
-        shutil.copytree(
-            os.path.join(root_dir, "src"),
-            tmpdir,
-            ignore=shutil.ignore_patterns("__pycache__"),
-            dirs_exist_ok=True,  # tmpdir exists already
-        )
-        # Copy ksc in as a subdirectory. Note this does not include ts2ks.
-        shutil.copytree(
-            os.path.join(root_dir, "knossos-ksc", "src", "python", "ksc"),
-            os.path.join(tmpdir, "ksc"),
-        )
+        copy_dir(target_dir=tmpdir, dir_name="src")
+        copy_dir(target_dir=tmpdir, dir_name="examples")
+
         yield tmpdir
--- src/bench/run_all_pytest_bench_azureml.py
+++ src/bench/run_all_pytest_bench_azureml.py
@@ -3,35 +3,45 @@
 #     > az account set -s Knossos
 #   You'll need to have at least Contributor access to the resource group (knossosamlrg).
 
+import os
 
 from azureml.core import ScriptRunConfig, Environment, Experiment
 from azureml.core.workspace import Workspace
 
-
-workspace = Workspace.from_config()
-workspace.get_details()
+from azureml_util import combined_source_directory
 
 
-experiment = Experiment(workspace, "benchmark_KSC")
+def main():
+    workspace = Workspace.from_config()
+    workspace.get_details()
 
-cluster = workspace.compute_targets["smallcpucluster"]
+    experiment = Experiment(workspace, "benchmark_KSC")
+
+    cluster = workspace.compute_targets["smallcpucluster"]
+
+    bench_dir = os.path.dirname(os.path.abspath(__file__))
+    env = Environment.from_pip_requirements(
+        "benchmark_env", os.path.join(bench_dir, "requirements.txt")
+    )
+
+    # taken from .sh, can we share cleanly?
+    BENCH = "pytest src/bench/ \
+            --benchmark-autosave --benchmark-max-time=5.0\
+            --benchmark-name=short --benchmark-sort=name --benchmark-group-by=group,func\
+            --benchmark-columns=median,iqr,outliers,mean,stddev,min,max,iterations,rounds"
+
+    sqrl_command = BENCH + " --modulepath=examples/dl-capsule/sqrl --benchmarkname=sqrl"
+
+    with combined_source_directory() as source_dir:
+        config = ScriptRunConfig(
+            source_directory=source_dir,
+            compute_target=cluster,
+            command=[sqrl_command],
+            environment=env,
+        )
+
+        script_run = experiment.submit(config)
 
 
-env = Environment.from_pip_requirements("benchmark_env", "requirements.txt")
-
-# taken from .sh, can we share cleanly?
-BENCH = "pytest src/bench/ \
-        --benchmark-autosave --benchmark-max-time=5.0\
-        --benchmark-name=short --benchmark-sort=name --benchmark-group-by=group,func\
-        --benchmark-columns=median,iqr,outliers,mean,stddev,min,max,iterations,rounds"
-
-sqrl_command = BENCH + "--modulepath=examples/dl-capsule/sqrl --benchmarkname=sqrl"
-
-config = ScriptRunConfig(
-    source_directory="../../",
-    compute_target=cluster,
-    command=[sqrl_command],
-    environment=env,
-)
-
-script_run = experiment.submit(config)
+if __name__ == "__main__":
+    main()
