f6a557d475a3b05826ee9b455bb03c405cf8776f
David Collier <david.collier@microsoft.com>
2021-08-04 11:14:09 +0100

    Add file missing from previous commits

---
+++ src/runtime/knossos-entry-points-python.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "knossos-entry-points.h"
+
+namespace ks {
+namespace entry_points {
+
+template<typename KsElementType, typename EntryPointElementType>
+struct Converter<ks::tensor<1, KsElementType>, std::vector<EntryPointElementType>>
+{
+  static ks::tensor<1, KsElementType> to_ks(std::vector<EntryPointElementType> const& arg) {
+    ks::tensor<1, KsElementType> ks_arg(&g_alloc, arg.size());
+    for (int i = 0; i != ks_arg.size(); ++i) {
+      ks_arg[i] = convert_argument<KsElementType>(arg[i]);
+    }
+    return ks_arg;
+  }
+
+  static std::vector<EntryPointElementType> from_ks(ks::tensor<1, KsElementType> const& ks_ret) {
+    std::vector<EntryPointElementType> ret;
+    for (int i = 0; i != ks_ret.size(); ++i) {
+      ret.push_back(convert_return_value<EntryPointElementType>(ks_ret[i]));
+    }
+    return ret;
+  }
+};
+
+template<typename KsType>
+struct PurePythonEntryPointType
+{
+  using type = KsType;
+};
+
+template<typename ...KsTypes>
+struct PurePythonEntryPointType<ks::Tuple<KsTypes...>>
+{
+  using type = std::tuple<typename PurePythonEntryPointType<KsTypes>::type...>;
+};
+
+template<typename KsElementType>
+struct PurePythonEntryPointType<ks::tensor<1, KsElementType>>
+{
+  using type = std::vector<typename PurePythonEntryPointType<KsElementType>::type>;
+};
+
+template<typename RetType, typename... ParamTypes>
+auto python_entry_point(RetType(*f)(ks::allocator*, ParamTypes...)) {
+  return [f](typename PurePythonEntryPointType<ParamTypes>::type ...params) {
+    return convert_return_value<typename PurePythonEntryPointType<RetType>::type>(
+      f(&g_alloc, convert_argument<ParamTypes>(params)...)
+    );
+  };
+}
+
+}
+}
