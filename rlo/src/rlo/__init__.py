40986250e476d460e831284f6552d3d162ee805b
Alan Lawrence <allawr@microsoft.com>
2021-08-09 16:25:28 +0100

    RLO: make train_over_expressions.py runnable (rm gitmodule, fix sys.path) (#1002)
    "cd rlo; python src/train_over_expressions.py fewer_simplify_rules"

--- rlo/src/rlo/__init__.py
+++ rlo/src/rlo/__init__.py
@@ -3,6 +3,6 @@ import os
 
 sys.path.append(
     os.path.join(
-        os.path.dirname(__file__), os.pardir, os.pardir, "knossos-ksc", "src", "python"
+        os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, "src", "python"
     )
 )
