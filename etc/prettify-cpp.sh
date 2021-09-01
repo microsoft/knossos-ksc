441bd1aa1de6ed28d1238446e33aeb3f0260942d
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2021-09-01 16:21:35 +0100

    Comments

--- etc/prettify-cpp.sh
+++ etc/prettify-cpp.sh
@@ -16,10 +16,10 @@
 
 sed -r\
  -e 's/\$alloc/alloc/g'\
- -e 's/^([ \t]+)ty\$[a-zA-Z0-9_$]+/\1auto/g'\
- -e 's/rev\$/Grad_/g'\
- -e 's/\bc\$/c/g'\
- -e 's/\$a[A-Za-z0-9_$]+//g'\
+ -e 's/^([ \t]+)ty\$[a-zA-Z0-9_$]+/\1auto/g' `# ty$rev$foo fred = -> auto fred = `\
+ -e 's/rev\$/rev$_/g'\          `# rev$foo -> rev$_foo`\
+ -e 's/\bc\$/c/g'\              `# c$18  ->  c18 `\
+ -e 's/\$a[A-Za-z0-9_$]+//g'\   `# Argument type mangling`\
  -e 's/ks::alloc_mark_t (.*) = alloc->mark\(\)/ks::mark \1(alloc)/' \
- -e 's/aten\$8.8/at::/g'\
+ -e 's/aten\$8.8/aten::/g'\
  $*
