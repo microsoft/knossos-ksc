77b8cf60939583ec498044a092cbbc1acf8171f7
David Collier <david.collier@microsoft.com>
2021-09-02 14:35:22 +0100

    Do I need to use python3?

--- test/builds/test_kscmlir_examples.sh
+++ test/builds/test_kscmlir_examples.sh
@@ -4,6 +4,6 @@ mkdir -p obj/test/kscmlir/examples
 
 for example in ex0; do
     echo "Testing example $example.ks"
-    python src/python/ksc/prune_and_compile.py test/ksc/$example.ks obj/test/kscmlir/examples/$example.kso
+    python3 src/python/ksc/prune_and_compile.py test/ksc/$example.ks obj/test/kscmlir/examples/$example.kso
     ./build/bin/ksc-mlir MLIR obj/test/kscmlir/examples/$example.kso > /dev/null
 done
