91fd22f8e9f390292da2946cc047b3baba58ae87
David Collier <david.collier@microsoft.com>
2021-09-02 14:51:51 +0100

    Set PYTHONPATH?

--- test/builds/test_kscmlir_examples.sh
+++ test/builds/test_kscmlir_examples.sh
@@ -1,9 +1,12 @@
 set -e
 
+KNOSSOS=$1
+KSCPY=$KNOSSOS/src/python
+
 mkdir -p obj/test/kscmlir/examples
 
 for example in ex0; do
     echo "Testing example $example.ks"
-    python3 src/python/ksc/prune_and_compile.py test/ksc/$example.ks obj/test/kscmlir/examples/$example.kso
+    PYTHONPATH=$KSCPY python3 src/python/ksc/prune_and_compile.py test/ksc/$example.ks obj/test/kscmlir/examples/$example.kso
     ./build/bin/ksc-mlir MLIR obj/test/kscmlir/examples/$example.kso > /dev/null
 done
