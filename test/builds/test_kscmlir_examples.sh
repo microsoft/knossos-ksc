872d9d92f3c40f6198efb3878bb9000388596282
David Collier <david.collier@microsoft.com>
2021-09-02 14:08:38 +0100

    Prune unused defs in prelude before testing with ksc-mlir

--- test/builds/test_kscmlir_examples.sh
+++ test/builds/test_kscmlir_examples.sh
@@ -4,10 +4,6 @@ mkdir -p obj/test/kscmlir/examples
 
 for example in ex0; do
     echo "Testing example $example.ks"
-    ./build/bin/ksc --generate-cpp \
-        --ks-source-file src/runtime/prelude.ks \
-        --ks-source-file test/ksc/$example.ks \
-        --ks-output-file obj/test/kscmlir/examples/$example.kso \
-        --cpp-output-file /dev/null
+    python src/python/ksc/prune_and_compile.py test/ksc/$example.ks obj/test/kscmlir/examples/$example.kso
     ./build/bin/ksc-mlir MLIR obj/test/kscmlir/examples/$example.kso > /dev/null
 done
