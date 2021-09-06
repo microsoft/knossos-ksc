3f3f76f12e1645e3f37df7e8160298bcd2e483b6
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-06 11:06:24 +0000

    Restore stax

--- src/python/ksc/backends/jax.py
+++ src/python/ksc/backends/jax.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as np
+import jax.experimental.stax as stax
 
 # Use relative import to work around a python 3.6 issue
 # https://stackoverflow.com/questions/57615877/importing-difference-in-python3-6-and-python3-7
