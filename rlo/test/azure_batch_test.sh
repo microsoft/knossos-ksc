1acacb98badab81c8b71f929478022685593ac8e
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-07 14:25:06 +0000

    Split out pytest run from other AzureBatch

--- rlo/test/azure_batch_test.sh
+++ rlo/test/azure_batch_test.sh
@@ -7,6 +7,3 @@ bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_stat
 bash ./test/gpu_test.sh
 bash ./test/ray_test.sh
 
-# Many of these tests also run in QuickTest and the pytest portion of BuildAndTest. However, those run on CPU.
-# we only really care about the medium tests (which are also executed by BuildAndTest but on CPU).
-pytest test/rlo/ --durations=5
