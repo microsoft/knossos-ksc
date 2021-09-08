deb16ce5c5b93d18d3f4c0ec7750dafc14f94bbb
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-08 10:44:22 +0000

    mkdir results

--- rlo/test/azure_batch_pytest.sh
+++ rlo/test/azure_batch_pytest.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 echo 'azure_batch_pytest.sh: start'
+mkdir -p results
 
 # pytest test/rlo/ --quick --durations=5
 
--- rlo/test/azure_batch_test.sh
+++ rlo/test/azure_batch_test.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 echo 'azure_batch_test.sh: start'
+mkdir -p results
 
 # Pytorch is nondeterministic on GPU, so don't include it here.
 # bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
