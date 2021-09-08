8083807f063df0968af7d86b819840c9d79f4676
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-08 08:38:25 +0000

    Make quick for testing

--- rlo/test/azure_batch_test.sh
+++ rlo/test/azure_batch_test.sh
@@ -5,5 +5,5 @@ set -x
 # Pytorch is nondeterministic on GPU, so don't include it here.
 bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
 bash ./test/gpu_test.sh
-bash ./test/ray_test.sh
+# bash ./test/ray_test.sh
 
