d75734aa96ee5ba142b596156086f4864ee505eb
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2021-09-09 01:00:00 +0100

    docs

--- doc/make.sh
+++ doc/make.sh
@@ -5,6 +5,8 @@ python -c 'import relu3 ; relu3.plot_relu3_via_knossos("../build/doc/relu3-grad-
 
 # make relu3.cpp
 relu_cpp=../build/torch_extensions/KscStub_VSelem___main___relu3/ks-main.cpp
-sh ../etc/prettify-cpp.sh < $relu_cpp | egrep -B 1 -A 20 '^ty.relu3 relu3\(' | tee sphinx/examples/relu3.cpp
+sh ../etc/prettify-cpp.sh < $relu_cpp |\
+ egrep -B 1 -A 20 '^ty.relu3 relu3\(' |\
+ tee ../build/doc/relu3.cpp
 
 sphinx-build sphinx ../build/doc
