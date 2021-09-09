97fffca8be7d3db0f24cf373b361844c8ff8a1d4
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-09 14:22:39 +0000

    rm gpu_test

--- rlo/test/azure_batch_test.sh
+++ rlo/test/azure_batch_test.sh
@@ -7,7 +7,7 @@ du -ha > du.txt
 
 # Pytorch is nondeterministic on GPU, so don't include it here.
 bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
-bash ./test/gpu_test.sh
+#bash ./test/gpu_test.sh
 bash ./test/ray_test.sh
 
 # Many of these tests also run in QuickTest and the pytest portion of BuildAndTest. However, those run on CPU.
