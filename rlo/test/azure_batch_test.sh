c9af65cafdf35f6bf871447a0bb66366d422c4e6
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-09 08:02:21 +0000

    temporarily remove actual tests

--- rlo/test/azure_batch_test.sh
+++ rlo/test/azure_batch_test.sh
@@ -2,10 +2,11 @@
 set -e
 set -x
 
+echo azure_batch_test start
 # Pytorch is nondeterministic on GPU, so don't include it here.
-bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
-bash ./test/gpu_test.sh
-bash ./test/ray_test.sh
+#bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
+#bash ./test/gpu_test.sh
+#bash ./test/ray_test.sh
 
 # Many of these tests also run in QuickTest and the pytest portion of BuildAndTest. However, those run on CPU.
 # we only really care about the medium tests (which are also executed by BuildAndTest but on CPU).
