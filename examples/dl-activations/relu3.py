44ccd2788152f63e04bc1b1c22853021b0259c1c
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2021-09-09 01:12:19 +0100

    Add nnModule, mnist, Lambda

--- examples/dl-activations/relu3.py
+++ examples/dl-activations/relu3.py
@@ -1,7 +1,11 @@
-import torch
+# fmt: off
+# import torch
 from torch import nn
 import os
 from collections import OrderedDict
+
+import torch
+
 from ksc import utils
 import ksc.compile
 import ksc.expr as expr
@@ -13,8 +17,7 @@ from ksc.torch_frontend import (
 import ksc.torch_frontend as knossos
 
 # DOC-KS
-@knossos.elementwise
-def vrelu3(x: float) -> float:
+def relu3(x: float) -> float:
     """
     Like ReLu, but smoother
     Like GeLu, but cheaper
@@ -25,10 +28,20 @@ def vrelu3(x: float) -> float:
         return 1 / 3 * x ** 3
     else:
         return x - 2 / 3
-
-
 # ENDDOC-KS
 
+vrelu3 = knossos.elementwise()(relu3)
+
+# DOC-KSV
+@knossos.vmap
+def vrelu3_knossos(x: float) -> float:
+    if x < 0.0:
+        return 0.0
+    elif x < 1.0:
+        return 1 / 3 * x ** 3
+    else:
+        return x - 2 / 3
+# ENDDOC-KSV
 
 # run-bench: PyTorch reference implementation
 # DOC-PTREF
@@ -39,8 +52,6 @@ def vrelu3_pytorch(x: torch.Tensor):
     val_1_inf = x - 2 / 3
 
     return mask0_1 * val_0_1 + mask1_inf * val_1_inf
-
-
 # ENDDOC-PTREF
 
 
@@ -67,6 +78,73 @@ if False:
     # This is something of a long story, related to tracing.
     # Sort of related discussion https://discuss.pytorch.org/t/custom-loss-function-error-element-0-of-tensors-does-not-require-grad-and-does-not-have-grad-fn/87944/16
 
+
+# run-bench: Define a range of values at which to call the methods
+def vrelu3_bench_configs():
+    yield torch.randn((16,))
+    yield torch.randn((255 * 255,))
+    yield torch.randn((1024 * 1024,))
+
+
+def plot_relu3(filename):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    t = np.arange(-3, 3, step=0.1)
+    plt.plot(t, [relu3(t) for t in t], "b")
+    plt.gca().set_aspect(1)
+    print(f"Saving figure to {filename}")
+    plt.savefig(filename, bbox_inches="tight")
+
+
+def plot_relu3_via_knossos(filename):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    t = torch.arange(-3, 3, step=0.1)
+
+    plt.plot(t, vrelu3(t), "b")
+    # Plot reference in dotted red to show that they match up
+    plt.plot(t, vrelu3_pytorch(t), "r--")
+
+    df = torch.ones_like(t)
+    # torch.autograd.functional.vjp(vrelu3, t, df)
+
+    derivatives = vrelu3.vjp(t, df)
+
+    plt.plot(t, derivatives, "b")
+
+    plt.gca().set_aspect(1)
+    print(f"Saving figure to {filename}")
+    plt.savefig(filename, bbox_inches="tight")
+
+
+def relu3_in_fcdnn():
+    # Initialize the model using nn.Sequential
+    model = nn.Sequential(
+        OrderedDict(
+            [
+                ("fc1", nn.Linear(784, 256)),
+                ("activation1", vrelu3),
+                ("fc2", nn.Linear(256, 128)),
+                ("bn2", nn.BatchNorm1d(num_features=128)),
+                ("activation2", vrelu3),
+                ("dropout", nn.Dropout(0.3)),
+                ("fc3", nn.Linear(128, 64)),
+                ("bn3", nn.BatchNorm1d(num_features=64)),
+                ("activation3", vrelu3),
+                ("logits", nn.Linear(64, 10)),
+                ("logsoftmax", nn.LogSoftmax(dim=1)),
+            ]
+        )
+    )
+
+    # Run training
+    # train_model(model)
+
+
+######################################################
+## C++ implementations for benchmarking
 
 embedded_cflags = ksc.compile.default_cflags
 
@@ -558,53 +636,6 @@ def vrelu3_cuda_init():
             return VReLu3Function.apply(input)
 
     return VReLu3()
-
-
-# run-bench: Define a range of values at which to call the methods
-def vrelu3_bench_configs():
-    yield torch.randn((16,))
-    yield torch.randn((255 * 255,))
-    yield torch.randn((1024 * 1024,))
-
-
-# yield torch.randn((256,256)) too slow to bench...
-
-
-def plot_relu3(filename):
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    t = torch.arange(-3, 3, step=0.1)
-
-    plt.plot(t, vrelu3(t), "b")
-    plt.plot(t, vrelu3_pytorch(t), "r--")
-    plt.gca().set_aspect(1)
-    print(f"Saving figure to {filename}")
-    plt.savefig(filename)
-
-
-def relu3_in_fcdnn():
-    # Initialize the model using nn.Sequential
-    model = nn.Sequential(
-        OrderedDict(
-            [
-                ("fc1", nn.Linear(784, 256)),
-                ("activation1", vrelu3),
-                ("fc2", nn.Linear(256, 128)),
-                ("bn2", nn.BatchNorm1d(num_features=128)),
-                ("activation2", vrelu3),
-                ("dropout", nn.Dropout(0.3)),
-                ("fc3", nn.Linear(128, 64)),
-                ("bn3", nn.BatchNorm1d(num_features=64)),
-                ("activation3", vrelu3),
-                ("logits", nn.Linear(64, 10)),
-                ("logsoftmax", nn.LogSoftmax(dim=1)),
-            ]
-        )
-    )
-
-    # Run training
-    # train_model(model)
 
 
 if __name__ == "__main__":
