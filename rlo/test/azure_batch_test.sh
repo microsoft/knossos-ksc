78f8a7e5e7fcd22d504cf307d4f90196236577fe
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-09 16:00:44 +0000

    Rm one more

--- rlo/test/azure_batch_test.sh
+++ rlo/test/azure_batch_test.sh
@@ -6,7 +6,7 @@ echo azure_batch_test start
 du -ha > du.txt
 
 # Pytorch is nondeterministic on GPU, so don't include it here.
-bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
+#bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
 #bash ./test/gpu_test.sh
 bash ./test/ray_test.sh
 
