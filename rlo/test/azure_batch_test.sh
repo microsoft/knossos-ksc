f660ee1c3a81019b2ebe8fe7acb6debb374e31c3
Andrew Fitzgibbon <awf@microsoft.com>
2021-09-09 11:04:43 +0000

    Restore RM pytest

--- rlo/test/azure_batch_test.sh
+++ rlo/test/azure_batch_test.sh
@@ -2,26 +2,26 @@
 set -e
 set -x
 
-echo azure_batch_test start
-du -ha > du.txt
+# echo azure_batch_test start
+# du -ha > du.txt
 # Pytorch is nondeterministic on GPU, so don't include it here.
 bash ./test/determinism_test.sh --tensorflow --output_keep_prob 1.0 --graph_state_keep_prob 1.0
+bash ./test/gpu_test.sh
+bash ./test/ray_test.sh
 
 # Many of these tests also run in QuickTest and the pytest portion of BuildAndTest. However, those run on CPU.
 # we only really care about the medium tests (which are also executed by BuildAndTest but on CPU).
 #pytest test/rlo/ --durations=5
 
-echo "azure_batch_test.sh: New files:"
-du -ha | diff -c du.txt -
-echo "azure_batch_test.sh: New files done"
-
-bash ./test/gpu_test.sh
-bash ./test/ray_test.sh
+# echo "azure_batch_test.sh: New files:"
+# du -ha | diff -c du.txt -
+# echo "azure_batch_test.sh: New files done"
 
 
-echo "azure_batch_test.sh: New files:"
-du -ha | diff -c du.txt -
-echo "azure_batch_test.sh: New files done"
+
+# echo "azure_batch_test.sh: New files:"
+# du -ha | diff -c du.txt -
+# echo "azure_batch_test.sh: New files done"
 
 # next: remove lines here, does that break build?
 
